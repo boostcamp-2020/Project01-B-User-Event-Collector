0a48e9cf0e383dc75ea1b9fb4d7128cb755fa8de
Sue Cho <suejeong97@naver.com>
2020-11-23 21:06:31 +0900

    [iOS] Feat : TrackRow 화면구성 #10
    - 앨범 사진, 곡명, 가수 이름을 표현할 뷰 구현
    - 앨범 사진 데이터 추가

---
+++ MiniVibe/MiniVibe/Views/TrackRow.swift
@@ -0,0 +1,20 @@
+//
+//  TrackRow.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/11/23.
+//
+
+import SwiftUI
+
+struct TrackRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrackRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackRow()
+    }
+}
