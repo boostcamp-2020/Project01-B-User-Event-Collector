32058069d52e69a9e5c5442aafc8bbf03a7706c8
Sue Cho <suejeong97@naver.com>
2020-11-23 20:28:54 +0900

    [iOS] Feat : 프로젝트 생성 #8
    - SwiftLint 적용
    - SwiftUI 선택

---
+++ MiniVibe/MiniVibe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
