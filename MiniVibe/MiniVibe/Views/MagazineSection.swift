a25c6512af8965d3832b661099e3cd3f956cb831
Sue Cho <suejeong97@naver.com>
2020-11-24 18:48:13 +0900

    [iOS] Feat : 매거진 모아보기 뷰 구현 #18
    - 매거진 item을 활용하여 스크롤 가능한 매거진 모아보기 뷰 구현

--- MiniVibe/MiniVibe/Views/MagazineSection.swift
+++ MiniVibe/MiniVibe/Views/MagazineSection.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct MagazineSection: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SectionTitle()
+            GeometryReader { geometry in
+                let width = geometry.size.width
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: width * 0.03) {
+                        ForEach(0..<10) { _ in
+                            MagazineItem()
+                                .frame(width: width * 0.9)
+                        }
+                    }
+                    .padding(.horizontal, width * 0.03)
+                }
+            }
+        }
     }
 }
 
