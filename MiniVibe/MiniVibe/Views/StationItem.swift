9c1cff4465333190ceb642838e338b99a769bb80
Sue Cho <suejeong97@naver.com>
2020-11-24 17:27:32 +0900

    [iOS] Feat : DJ 스테이션 Item 뷰 구현 #23
    - 스테이션 사진 추가
    - 썸네일, 재생 아이콘 추가

---
+++ MiniVibe/MiniVibe/Views/StationItem.swift
@@ -0,0 +1,20 @@
+//
+//  StationItem.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/11/24.
+//
+
+import SwiftUI
+
+struct StationItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StationItem_Previews: PreviewProvider {
+    static var previews: some View {
+        StationItem()
+    }
+}
