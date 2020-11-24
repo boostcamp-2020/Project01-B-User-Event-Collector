c52aa616be3a1fd45ff8db9fd5ebb6204f771a11
YimJiYoung <chiy323@gmail.com>
2020-11-24 17:23:37 +0900

    [FE] feat: Button 컴포넌트 작성 #37
    - styeld-components 설치
    - varient(primary/default)에 따라 다른 스타일을 적용하는 StyleButton 감싼 Button 컴포넌트 구현

---
+++ client/src/components/atoms/Button/Button.styles.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const getButtonStyle = (props) => {
+  let color; let borderColor; let backgroundColor;
+
+  switch (props.variant) {
+    case 'primary':
+      color = 'white';
+      borderColor = '#FF1150';
+      backgroundColor = '#FF1150';
+      break;
+    default:
+      color = 'black';
+      borderColor = '#D7D7D7';
+      backgroundColor = 'white';
+      break;
+  }
+
+  return `
+    color: ${color};
+    border: 1px solid ${borderColor};
+    background-color: ${backgroundColor};
+  `;
+};
+
+export const StyledButton = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  padding: 9px 15px;
+
+  ${(props) => getButtonStyle(props)};
+  border-radius: 4px;
+  box-shadow: none;
+  cursor: pointer;
+  font-size: 15px;
+  line-height: 1;
+  outline: none;
+  text-decoration: none;
+  transition: all 0.15s ease;
+  white-space: nowrap;
+`;
+
+export default StyledButton;
