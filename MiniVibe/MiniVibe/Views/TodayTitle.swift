ca7e03d100be739c1d0e9dd94583cbd5b5a01959
TTOzzi <devttozzi@gmail.com>
2020-11-24 15:36:43 +0900

    [iOS] Feat: TodayTitle 뷰 구현 #12
    내돈내듣을 버튼으로 구현
    프로필 사진을 표시할 이미지 뷰 구현

---
+++ MiniVibe/MiniVibe/Views/TodayTitle.swift
@@ -0,0 +1,37 @@
+//
+//  TodayTitle.swift
+//  MiniVibe
+//
+//  Created by TTOzzi on 2020/11/24.
+//
+
+import SwiftUI
+
+struct TodayTitle: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Text("#내돈내듣 VIBE")
+                    .foregroundColor(.black)
+                    .font(.title)
+                    .fontWeight(.heavy)
+            }
+            
+            Spacer()
+            
+            Image(systemName: "person")
+                .clipShape(Circle())
+                .font(.system(size: 24))
+                .frame(width: 60, height: 60)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TodayTitle_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayTitle()
+    }
+}
