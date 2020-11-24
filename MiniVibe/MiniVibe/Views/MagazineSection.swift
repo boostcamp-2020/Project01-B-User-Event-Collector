90115df22dc9d021c481cdf9dda3b575043f385a
Sue Cho <suejeong97@naver.com>
2020-11-24 18:39:54 +0900

    [iOS] Feat : 매거진 모아보기 뷰 구현 #18
    - 매거진 item을 활용하여 좌우로 scrollable 한 화면 구현

---
+++ MiniVibe/MiniVibe/Views/MagazineSection.swift
@@ -0,0 +1,20 @@
+//
+//  MagazineSection.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/11/24.
+//
+
+import SwiftUI
+
+struct MagazineSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MagazineSection_Previews: PreviewProvider {
+    static var previews: some View {
+        MagazineSection()
+    }
+}
