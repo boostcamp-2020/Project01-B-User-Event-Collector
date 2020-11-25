8b2287f59923dcc642cbe76095b4de5a12d9e2aa
Sue Cho <34840140+Sueaty@users.noreply.github.com>
2020-11-25 16:43:04 +0900

    [iOS] Feat : 더보기 화면의 list의 row 구현 #27
    - 썸네일, 제목, description(option), 부제목을 표시할 직사각형 모양의 Row 구현

--- MiniVibe/MiniVibe/Views/ListRow.swift
+++ MiniVibe/MiniVibe/Views/ListRow.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ListRow: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 15) {
+            Image("playListThumbnail")
+                .resizable()
+                .frame(width: 60, height: 60)
+            VStack(alignment: .leading, spacing: 5) {
+                VStack(alignment: .leading) {
+                    Text("아시아 아티스트 어워즈 2020")
+                        .font(.system(size: 16))
+                    Text("일할 때, 공부할 때 꼭 음악을 들어야 집중이 잘 된다면 이 플레이리스트를 추천할게요!")
+                        .font(.system(size: 12))
+                        .foregroundColor(.secondary)
+                }
+                Text("아시아 아티스트 어워즈 2020")
+                    .font(.system(size: 12))
+                    .foregroundColor(.secondary)
+            }
+            .lineLimit(1)
+        }
     }
 }
 
