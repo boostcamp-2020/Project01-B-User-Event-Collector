6c4e213856141cee96adc5bcc87bbc8ecfca0212
Sue Cho <suejeong97@naver.com>
2020-11-25 13:45:43 +0900

    [iOS] Feat : 컨텐츠 모아보기 뷰 수정 #24
    - GeometryReader 제거 및 이미지 비율 수정

---
+++ MiniVibe/MiniVibe/Views/ContentSection.swift
@@ -0,0 +1,30 @@
+//
+//  ContentSection.swift
+//  MiniVibe
+//
+//  Created by TTOzzi on 2020/11/24.
+//
+
+import SwiftUI
+
+struct ContentSection: View {
+    let width: CGFloat
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<5) { _ in
+                    ContentItem()
+                        .frame(width: width * 0.94)
+                }
+            }
+            .padding(.horizontal, 8)
+        }
+    }
+}
+
+struct ContentSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentSection(width: 200)
+    }
+}
