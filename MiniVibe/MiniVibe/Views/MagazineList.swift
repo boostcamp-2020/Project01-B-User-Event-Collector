2f3660436c81515eb97a54247d5b3516d721d326
Sue Cho <suejeong97@naver.com>
2020-11-25 16:35:39 +0900

    [iOS] Feat : 더보기 화면의 list 구현 #26, #30, #34
    - Navigation bar title을  inline으로 작성
    - LazyVGrid로 목록 구성

---
+++ MiniVibe/MiniVibe/Views/MagazineList.swift
@@ -0,0 +1,20 @@
+//
+//  MagazineList.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/11/25.
+//
+
+import SwiftUI
+
+struct MagazineList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MagazineList_Previews: PreviewProvider {
+    static var previews: some View {
+        MagazineList()
+    }
+}
