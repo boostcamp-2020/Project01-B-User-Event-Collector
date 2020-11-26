b1019847351cd195ab4b94c9b2d67839cd669b22
YimJiYoung <chiy323@gmail.com>
2020-11-27 08:45:44 +0900

    [FE] Chore: tsconfig alias를 storybook에도 사용하도록 모듈 설치 및 설정
    - tsconfig paths webpack plugin 모듈 설치
    - storybook 설정 추가

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin  = require('tsconfig-paths-webpack-plugin')
+
 module.exports = {
   "stories": [
     "../**/*.stories.mdx",
@@ -6,5 +8,9 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  "webpackFinal": async (config) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
+  },
 }
