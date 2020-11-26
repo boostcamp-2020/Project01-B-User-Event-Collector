dae3383fa8f81ff92e585c68765f3086dfa301be
YimJiYoung <chiy323@gmail.com>
2020-11-26 11:26:56 +0900

    [FE] Feat: Button 컴포넌트에 icon props 추가
    - MenuLink 컴포넌트와 같은 방식으로 icon props를 받아 존재할 경우 IconWrapper로 감싸줘서 렌더

--- client/src/components/atoms/Button/Button.tsx
+++ client/src/components/atoms/Button/Button.tsx
@@ -1,14 +1,27 @@
-import React, { ReactNode, MouseEvent } from 'react';
+import React, { ReactNode, MouseEvent, ComponentType } from 'react';
+import styled from 'styled-components';
 import StyledButton from './Button.styles';
 
 interface ButtonProps {
     children: ReactNode;
+    icon?: ComponentType;
     onClick?: (e: MouseEvent<HTMLElement>) => void;
     variant?: 'primary';
 }
 
-const Button = ({ children, onClick, variant }: ButtonProps) => (
+const IconWrapper = styled.div`
+    margin-right: 10px;
+`;
+
+const Button = ({
+  children, onClick, icon: Icon, variant,
+}: ButtonProps) => (
   <StyledButton variant={variant} onClick={onClick}>
+    { Icon && (
+    <IconWrapper>
+      <Icon />
+    </IconWrapper>
+    )}
     {children}
   </StyledButton>
 );
