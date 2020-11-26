c9530a6d966d7ed62b7ff7e1d4180b6fe226ea33
YimJiYoung <chiy323@gmail.com>
2020-11-27 00:21:19 +0900

    [FE] Feat: UserProfileMenu 컴포넌트 구현
    - 사이드바의 userProfileMenu 구현
    - userProfileMenu에서 사용가능하도록 MenuLink, DropdownMenu 수정

---
+++ client/components/organisms/UserProfileMenu/UserProfileMenu.tsx
@@ -0,0 +1,56 @@
+import React, { MouseEvent } from 'react';
+import { useRouter } from 'next/router'
+import styled from 'styled-components';
+import MenuLink from '@components/atoms/MenuLink';
+import DropdownMenu from '@components/molecules/DropdownMenu';
+import Avatar from '@material-ui/core/Avatar';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+interface UserProfileMenuProps {
+    user?: {
+        id: string,
+        name: string,
+        profileUrl?: string,
+    };
+}
+
+const IconWrapper = styled.div`
+    margin-right: 10px;
+`;
+
+const UserProfileMenu = ({ user }: UserProfileMenuProps) => {
+  const router = useRouter();
+
+  const defaultImage = 'https://ssl.pstatic.net/static/common/myarea/myInfo.gif?type=s33';
+  const dropdownItems = [{
+      content: 'My 멤버십',
+      handleClick: (e: MouseEvent<HTMLElement>) => router.push('/membership/my'),
+    }, {
+      content: '로그아웃',
+      handleClick: (e: MouseEvent<HTMLElement>) => console.log('로그아웃'), // TODO: 로그아웃 함수로 변경
+    }];
+
+  if (!user) {
+    return (
+      <MenuLink href="/login">
+        <IconWrapper>
+          <Avatar alt="profile" src={defaultImage} />
+        </IconWrapper>
+        로그인
+      </MenuLink>
+    );
+  }
+  return (
+    <DropdownMenu id="user-profile" menuItems={dropdownItems}>
+    <MenuLink href="">
+          <IconWrapper>
+            <Avatar alt="profile" src={user.profileUrl ? user.profileUrl : defaultImage} />
+          </IconWrapper>
+          {user.name}
+          <ArrowDropDownIcon />
+    </MenuLink>
+    </DropdownMenu>
+  );
+};
+
+export default UserProfileMenu;
