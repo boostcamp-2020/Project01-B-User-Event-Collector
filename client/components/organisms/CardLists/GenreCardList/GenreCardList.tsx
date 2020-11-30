013bb9f74e55e77e51ec486cf71108dcd192a671
shany <shany6310@gmail.com>
2020-11-30 23:39:14 +0900

    [FE] fix: 장르 카드 번들링 함수 수정 #159
    - 카드의 개수가 3의 배수가 아닐 경우 i+1, i+2 값이 undefined 인 경우에 랜더링 되지 않도록 삼항 연산자 추가
    - 카드가 리스트의 위쪽부터 채워질 수 있도록 verical-align 속성 top으로 변경

--- client/components/organisms/CardLists/GenreCardList/GenreCardList.tsx
+++ client/components/organisms/CardLists/GenreCardList/GenreCardList.tsx
@@ -4,7 +4,7 @@ import GenreCard from '@components/molecules/GenreCard/GenreCard';
 
 // TODO : DB 에서 장르 내용 받아오기
 const cards = [];
-for (let i = 0; i < 15; i++) {
+for (let i = 0; i < 13; i++) {
     const title = `test${i}`;
     const href = '#';
     const color = `#${Math.round(Math.random() * 0xffffff).toString(16)}`;
@@ -13,6 +13,7 @@ for (let i = 0; i < 15; i++) {
         href,
         color,
     };
+    cards.push(card);
 }
 
 // TODO : refactor
@@ -23,8 +24,12 @@ const getCardBundle = (cards) => {
         bundle.push(
             <CardBundle>
                 <GenreCard title={cards[i].title} href={cards[i].href} color={cards[i].color} />
-                <GenreCard title={cards[i + 1].title} href={cards[i + 1].href} color={cards[i + 1].color} />
-                <GenreCard title={cards[i + 2].title} href={cards[i + 2].href} color={cards[i + 2].color} />
+                {cards[i + 1] ? (
+                    <GenreCard title={cards[i + 1].title} href={cards[i + 1].href} color={cards[i + 1].color} />
+                ) : null}
+                {cards[i + 2] ? (
+                    <GenreCard title={cards[i + 2].title} href={cards[i + 2].href} color={cards[i + 2].color} />
+                ) : null}
             </CardBundle>,
         );
     }
@@ -43,6 +48,7 @@ const CardBundle = styled.div`
     display: inline-block;
     box-sizing: border-box;
     margin-right: 16px;
+    vertical-align: top;
 `;
 
 const GenreCardList = () => <StyledListContainer>{getCardBundle(cards)}</StyledListContainer>;
