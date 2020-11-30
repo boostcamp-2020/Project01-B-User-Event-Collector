91ee959fe8ac74dff4fcb943c9677986106d7366
shany <shany6310@gmail.com>
2020-12-01 01:08:29 +0900

    [FE] fix: Magazine card props 모듈로 변경
    - MagazineCardProps를 매거진 리스트 컴포넌트에서도 사용하기 위해 인터페이스 모듈에 추가

--- client/components/organisms/CardLists/MagazineList/MagazineList.stories.tsx
+++ client/components/organisms/CardLists/MagazineList/MagazineList.stories.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import MagazineList from './MagazineList';
-
+import { MagazineSort } from '@interfaces/props';
 export default {
     title: 'MagazineList',
     component: MagazineList,
 };
+const Magazinesdata = Array(9).fill({
+    src: 'https://musicmeta-phinf.pstatic.net/artist/002/826/2826154.jpg?type=ff300_300&v=20191231151906',
+    href: 'localhost:3000',
+    title: `이 주의 디깅 #77 
+    이영지 새 앨범 발표`,
+    date: '2020.11.25',
+    sort: MagazineSort.main,
+});
 
-export const Defalut = () => <MagazineList />;
+export const Defalut = () => <MagazineList items={Magazinesdata} />;
--- client/interfaces/props.ts
+++ client/interfaces/props.ts
@@ -17,3 +17,10 @@ export interface NewsCardProps {
     newsHref: string,
     title: string,
 };
+export interface MagazineCardProps {
+    src: string,
+    href: string,
+    title: string,
+    date: string,
+    sort: 'todayMagazine' | 'normalMagazine'
+};
