c08a02b654522311970e6cb0f90769ec6572ddc8
TTOzzi <devttozzi@gmail.com>
2020-12-01 01:41:12 +0900

    [iOS] Feat: 재생화면 곡 정보 표시 뷰 구현 #109 #114
    재생화면에서 곡 정보를 표시하는 뷰 구현
    음원 재생시 썸네일 크기를 키우고 가사를 표시

---
+++ MiniVibe/MiniVibe/Views/Player.swift
@@ -0,0 +1,67 @@
+//
+//  Player.swift
+//  MiniVibe
+//
+//  Created by TTOzzi on 2020/11/30.
+//
+
+import SwiftUI
+
+struct Player: View {
+    @State var isPlaying = false
+    
+    var body: some View {
+        VStack {
+            PlayerHeader(title: "오늘 Top 100")
+            
+            Spacer()
+            
+            PlayerThumbnail(image: "playListThumbnail",
+                            lyrics: "가사가사가사가사\n가사가사\n가사\n가사가사가사가사", isPlaying: $isPlaying)
+            
+            Spacer()
+            
+            PlayerControls(isPlaying: $isPlaying)
+                
+            Spacer()
+            
+            footer
+        }
+        .padding(16)
+    }
+    
+    var footer: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Image(systemName: "airplayaudio")
+                    .foregroundColor(.secondary)
+            }
+            Spacer()
+            Button {
+                
+            } label: {
+                HStack(spacing: 2) {
+                    Text("미리듣기 중")
+                    Image(systemName: "info.circle")
+                }
+                .foregroundColor(.pink)
+                .font(.system(size: 12))
+            }
+            Spacer()
+            Button {
+                
+            } label: {
+                Image(systemName: "music.note.list")
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct Player_Previews: PreviewProvider {
+    static var previews: some View {
+        Player()
+    }
+}
--- MiniVibe/MiniVibe/Views/PlayerThumbnail.swift
+++ MiniVibe/MiniVibe/Views/PlayerThumbnail.swift
@@ -10,24 +10,27 @@ import SwiftUI
 struct PlayerThumbnail: View {
     let image: String
     let lyrics: String
+    @Binding var isPlaying: Bool
     
     var body: some View {
         VStack {
             Image(image)
                 .resizable()
                 .aspectRatio(1, contentMode: .fit)
+                .scaleEffect(isPlaying ? 1 : 0.9)
             Text(lyrics)
-                .font(.system(size: 12))
+                .font(.system(size: 14))
                 .foregroundColor(.secondary)
                 .lineLimit(2)
+                .opacity(isPlaying ? 1 : 0)
         }
-        .padding(.vertical, 24)
+        .animation(.easeInOut)
     }
 }
 
 struct PlayerThumbnail_Previews: PreviewProvider {
     static var previews: some View {
         PlayerThumbnail(image: "playListThumbnail",
-                        lyrics: "가사가사가사가사\n가사가사\n가사\n가사가사가사가사")
+                        lyrics: "가사가사가사가사\n가사가사\n가사\n가사가사가사가사", isPlaying: .constant(false))
     }
 }
