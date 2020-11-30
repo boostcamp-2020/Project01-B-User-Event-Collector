a29f467d0a212c2b53dae5141f8fb7263bff9f59
shany <shany6310@gmail.com>
2020-12-01 01:22:15 +0900

    [FE] feat: 매거진 리스트 CSS 컴포넌트 생성 #161
    - 매거진 리스트에 적용할 CSS styled-component 생성 및 적용

--- client/components/organisms/CardLists/MagazineList/MagazineList.tsx
+++ client/components/organisms/CardLists/MagazineList/MagazineList.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import MagazineCard from '@components/organisms/Cards/MagazineCard';
 import { MagazineCardProps } from '@interfaces/props';
 interface MagazineListProps {
     items: MagazineCardProps[];
 }
-
+const ListContainer = styled.ul`
+    list-style: none;
+    padding: 0;
+    overflow: hidden;
+`;
+const StyledList = styled.li`
+    float: left;
+    width: 33.333333%;
+    margin: 0;
+    box-sizing: border-box;
+    padding: 0 17px 39px 0;
+`;
 const MagazineList = ({ items }: MagazineListProps) => (
-    <ul>
+    <ListContainer>
         {items.map((item) => (
-            <li>
+            <StyledList>
                 <MagazineCard {...(item as MagazineCardProps)} />
-            </li>
+            </StyledList>
         ))}
-    </ul>
+    </ListContainer>
 );
 
 export default MagazineList;
