9e062c056fb9f6c150752973f22dcb254f5c8ae6
shany <shany6310@gmail.com>
2020-12-01 00:47:20 +0900

    [FE] test: 매거진 리스트 컴포넌트 storybook 테스트 코드 생성 #161
    - 매거진 리스트 컴포넌트 테스를 위한 storybook 테스트 코드 생성

---
+++ client/components/organisms/CardLists/MagazineList/MagazineList.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MagazineList from './MagazineList';
+
+export default {
+    title: 'MagazineList',
+    component: MagazineList,
+};
+
+export const Defalut = () => <MagazineList />;
