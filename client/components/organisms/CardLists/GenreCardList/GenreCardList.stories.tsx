bfe94ac07d5d4e4958a4f81a2d9e3272444dbde1
shany <shany6310@gmail.com>
2020-11-30 23:02:34 +0900

    [FE] feat: 장르 카드 리스트 컴포넌트 생성 및 storybook 테스트 추가 #159
    - 장르 카드 리스트 컴포넌트 생성 및 ui 테스트를 위한 storybook test 코드 추가

---
+++ client/components/organisms/CardLists/GenreCardList/GenreCardList.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import GenreCardList from './GenreCardList';
+
+export default {
+    title: 'GenreCardList',
+    component: GenreCardList,
+};
+
+export const Default = () => <GenreCardList />;
---
+++ client/components/organisms/CardLists/GenreCardList/GenreCardList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import GenreCard from '@components/molecules/GenreCard/GenreCard';
+
+const GenreCardList = () => (
+    <StyledListContainer>
+        <GenreCard />
+    </StyledListContainer>
+);
+
+export default GenreCardList;
