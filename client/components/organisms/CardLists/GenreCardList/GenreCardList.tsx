86e21ee6ec89b373450627de3aa7c79da1b25efc
shany <shany6310@gmail.com>
2020-11-30 23:04:24 +0900

    [FE] feat: 장르 카드를 묶는 함수 생성 #159
    - 장르카드를 3개씩 묶기 위한 번들러 함수 구현

--- client/components/organisms/CardLists/GenreCardList/GenreCardList.tsx
+++ client/components/organisms/CardLists/GenreCardList/GenreCardList.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import GenreCard from '@components/molecules/GenreCard/GenreCard';
 
-const GenreCardList = () => (
-    <StyledListContainer>
-        <GenreCard />
-    </StyledListContainer>
-);
+const getCardBundle = (cards) => {
+    const bundle = [];
+
+    for (let i = 0; i < cards.length; i += 3) {
+        bundle.push(
+            <CardBundle>
+                <GenreCard title={cards[i].title} href={cards[i].href} color={cards[i].color} />
+                <GenreCard title={cards[i + 1].title} href={cards[i + 1].href} color={cards[i + 1].color} />
+                <GenreCard title={cards[i + 2].title} href={cards[i + 2].href} color={cards[i + 2].color} />
+            </CardBundle>,
+        );
+    }
+    return bundle;
+};
+
+const GenreCardList = () => <StyledListContainer>{getCardBundle(cards)}</StyledListContainer>;
 
 export default GenreCardList;
