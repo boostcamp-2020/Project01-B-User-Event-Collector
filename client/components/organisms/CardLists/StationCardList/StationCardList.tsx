408dbeb5d7c321d5a01062d11a18b58b4c5fce0a
shany <shany6310@gmail.com>
2020-12-01 02:04:38 +0900

    [FE] feat: 스테이션 카드 리스트 styled-component 생성 #163
    - 스테이션 카드 리스트에 대한 styled-component 생성 및 적용

--- client/components/organisms/CardLists/StationCardList/StationCardList.tsx
+++ client/components/organisms/CardLists/StationCardList/StationCardList.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import { StationCardProps } from '@interfaces/props';
 import StationCard from '@components/organisms/Cards/StationCard/StationCard';
 
 interface StationCardListProps {
     items: StationCardProps[];
 }
+const ListContainer = styled.ul`
+    margin: 0;
+    list-style: none;
+    padding: 0;
+    overflow: hidden;
+`;
+
+const StyledList = styled.li`
+    box-sizing: border-box;
+    float: left;
+    padding: 0 16px 16px 0;
+    width: 196px;
+`;
 const StationCardList = ({ items }: StationCardListProps) => (
-    <ul>
+    <ListContainer>
         {items.map((item) => (
-            <li>
+            <StyledList>
                 <StationCard {...(item as StationCardProps)} />
-            </li>
+            </StyledList>
         ))}
-    </ul>
+    </ListContainer>
 );
 
 export default StationCardList;
