0df053fb769edd7095a887264f310f592a268c2b
mingd1023 <mingd1023@gmail.com>
2020-11-30 14:45:45 +0900

    [FE] Feat: 앨범 카드 구현

---
+++ client/components/organisms/Cards/AlbumCard/AlbumCard.tsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+import ContentsThumbnail from '../../../molecules/ContentsThumbnail/ContentsThumbnail';
+import A from '../../../atoms/A/A';
+import Text from '../../../atoms/Text/Text';
+
+const CardContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    width: 180px;
+    height: 260px;
+`;
+
+const ThumbnailContainer = styled.div`
+
+`;
+
+const TextContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    height: 50%;
+`;
+
+const TitelContainer = styled.div`
+    height: 75%;
+    padding: 10px 0px 10px 0px;
+`;
+
+const DescriptionContainer = styled.div`
+    height: 25%;
+    padding: 0px 0px 20px 0px;
+`;
+
+const StyledA = styled(A)`
+    font-size: 16px;
+`;
+
+const StyledText = styled(Text)`
+`;
+
+interface AlbumCardProps {
+    src: string,
+    href: string,
+    title: string,
+    artist: string
+};
+
+const AlbumCard = ( { title, artist, src, href }: AlbumCardProps ) => (
+    <CardContainer >
+        <ThumbnailContainer>
+            <ContentsThumbnail src = {src} href = {href} sort = "" />
+        </ThumbnailContainer>
+        <TextContainer>
+            <TitelContainer>
+                <StyledA href={href}>{title}</StyledA>
+            </TitelContainer>
+            <DescriptionContainer>
+                <StyledText variant = "primary">{artist}</StyledText>
+            </DescriptionContainer>
+        </TextContainer>
+    </CardContainer>
+)
+
+export default AlbumCard;
