90451521ae3207a25f019f02d1dd6899ae2c695e
mingd1023 <mingd1023@gmail.com>
2020-11-30 14:46:36 +0900

    [FE] Feat: 믹스테잎 카드 구현

---
+++ client/components/organisms/Cards/MixtapeCard/MixtapeCard.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+import ContentsThumbnail from '../../../molecules/ContentsThumbnail/ContentsThumbnail';
+import A from '../../../atoms/A/A';
+import Text from '../../../atoms/Text/Text';
+
+const CardContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    width: 180px;
+    height: 240px;
+`;
+
+const ThumbnailContainer = styled.div`
+
+`;
+
+const TextContainer = styled.div`
+    display: flex;
+    flex-flow: column;
+    height: 50%;
+`;
+
+const TitelContainer = styled.div`
+    height: 75%;
+    padding: 10px 0px 0px 0px;
+`;
+
+const DescriptionContainer = styled.div`
+    height: 25%;
+    padding: 0px 0px 30px 0px;
+`;
+
+const StyledA = styled(A)`
+    font-size: 14px;
+`;
+
+const StyledText = styled(Text)`
+    font-size: 13px;
+`;
+
+interface MixtapeCardProps {
+    src: string,
+    href: string,
+    title: string,
+    artist: string
+};
+
+const MixtapeCard = ( { title, artist, src, href }: MixtapeCardProps ) => (
+    <CardContainer >
+        <ThumbnailContainer>
+            <ContentsThumbnail src = {src} href = {href} sort = "" />
+        </ThumbnailContainer>
+        <TextContainer>
+            <TitelContainer>
+                <StyledA href={href}>{title}</StyledA>
+            </TitelContainer>
+            <DescriptionContainer>
+                <StyledText variant = "primary">{artist}</StyledText>
+            </DescriptionContainer>
+        </TextContainer>
+    </CardContainer>
+)
+
+export default MixtapeCard;
