24280a1a265a2871714f53d3e2fa5173d7268bc1
mingd1023 <mingd1023@gmail.com>
2020-12-01 21:31:21 +0900

    [FE] Test: 카드 스크롤 리스트 모든 경우에 대한 스토리 구현
    컨텐츠 카드 리스트를 새로 생성함에 따라,
    뉴스, 앨범, 아티스트, 믹스테잎, 플레이리스트 카드의 스크롤 리스트에 대한 스토리 구현

--- client/components/organisms/CardScrollList/CardScrollList.stories.tsx
+++ client/components/organisms/CardScrollList/CardScrollList.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CardScrollList from './CardScrollList';
 import { MagazineSort } from '@interfaces/props';
 import MagazineCardList from '@components/organisms/CardLists/MagazineList/MagazineList';
+import ContentsCardList from '@components/organisms/CardLists/ContentsCardList';
 
 export default {
     title: 'CardScrollList',
@@ -23,9 +24,66 @@ const Newsdata = Array(9).fill({
     title: `이영지가 새 앨범을 발표했습니다`,
 });
 
+const Albumdata = Array(9).fill({
+    src: "https://musicmeta-phinf.pstatic.net/album/005/102/5102890.jpg?type=r360Fll&v=20201123123608",
+    href: 'localhost:3000',
+    title: "Blue Skies",
+    artist: "Birdy"
+});
+
+const Artistdata = Array(9).fill({
+    name: '이영지',
+    src: 'https://musicmeta-phinf.pstatic.net/artist/002/826/2826154.jpg?type=ff300_300&v=20191231151906',
+    href: 'localhost:3000',
+});
+
+const Mixtapedata = Array(9).fill({
+    src: "https://vibeapp.music.naver.com/vibe/v1/cover/mix/3171155,2487724,3553414,635724/favorite/favorite/",
+    href: 'localhost:3000',
+    title: "나를 위한 믹스테잎",
+    artist: "Lana Del Rey, Dua Lipa, 이영지"
+});
+
+const Playlistdata = Array(9).fill({
+    src: "https://music-phinf.pstatic.net/20200504_183/1588567824216rHHs6_PNG/VIBE_%B0%F8%C5%EB_VibeAndChill.png",
+    href: 'localhost:3000',
+    title: "VIBE AND CHILL",
+    description: "VIBE"
+});
+
 // export const News = () => <CardScrollList type="news" items={Newsdata}/>
 export const Magazine = () => (
     <CardScrollList>
         <MagazineCardList variant="row" items={Magazinesdata} />
+    </CardScrollList>
+);
+
+export const News = () => (
+    <CardScrollList>
+        <ContentsCardList variant="news" items={Newsdata} />
+    </CardScrollList>
+);
+
+export const Album = () => (
+    <CardScrollList>
+        <ContentsCardList variant="album" items={Albumdata} />
+    </CardScrollList>
+);
+
+export const Artist = () => (
+    <CardScrollList>
+        <ContentsCardList variant="artist" items={Artistdata} />
+    </CardScrollList>
+);
+
+export const Mixtape = () => (
+    <CardScrollList>
+        <ContentsCardList variant="mixtape" items={Mixtapedata} />
+    </CardScrollList>
+);
+
+export const Playlist = () => (
+    <CardScrollList>
+        <ContentsCardList variant="playlist" items={Playlistdata} />
     </CardScrollList>
 );
