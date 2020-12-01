7d6dc82cb372a01a213cab493951b6a2be6ac439
mingd1023 <mingd1023@gmail.com>
2020-12-01 15:48:13 +0900

    [FE] Feat: 보관함에 필요한 카드 리스트 구현

---
+++ client/components/organisms/Library/LibraryCardList/LibraryCardList.styles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ListContainer = styled.div`
+    width: 960px;
+`;
+
+export const List = styled.ul`
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-flow: wrap;
+`;
+
+export const Item = styled.li`
+    width: 180px;
+    margin-left: 10px;
+    margin-bottom: 20px;
+`;
