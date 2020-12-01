3c7b2cdf021832399a417650eb5ad4a4a9368ca9
shany <shany6310@gmail.com>
2020-12-02 05:02:55 +0900

    [FE} refactor: styled-component 분리 #197
    - styled-component 들을 컴포넌트에서 분리
    - 중복되는 부분 공통 styled-component 로 분리

--- client/components/organisms/Cards/TrackRowCard/TrackRowCard.tsx
+++ client/components/organisms/Cards/TrackRowCard/TrackRowCard.tsx
@@ -1,86 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
 import A from '@components/atoms/A/A';
 import HiddenText from '@components/atoms/Text/HiddenText';
 import CheckBox from '@components/atoms/CheckBox/CheckBox';
 import TrackPlayButton from '@components/molecules/TrackPlayButton/TrackPlayButton';
-import ArtistLikeButton from '@components/molecules/ArtistLikeButton/ArtistLikeButton';
-const List = styled.li`
-    list-style: none;
-    margin: 0;
-    display: flex;
-    justify-content: space-between;
-`;
-const TrackLeft = styled.div`
-    display: flex;
-    height: 50px;
-    padding: 5px 0;
-    box-sizing: border-box;
-    width: 380px;
-`;
-const TrackMiddle = styled.div`
-    display: flex;
-    height: 50px;
-`;
-const TrackRight = styled.div`
-    display: flex;
-    padding: 15px 0;
-    box-sizing: border-box;
-    height: 50px;
-    width: 136px;
-    justify-content: space-between;
-`;
-const TrackPlayBtnContainer = styled.div`
-    padding-right: 7px;
-    height: 40px;
-`;
-const TrackTitle = styled.div`
-    padding: 13px 0 13px 7px;
-`;
-const TrackMiddleElem = styled.div`
-    width: 195px;
-    max-width: 195px;
-    padding: 15px 0 15px 30px;
-`;
-const Mp3 = styled.div`
-    &::after {
-        content: '';
-        background-image: linear-gradient(transparent, transparent),
-            url(https://vibe.naver.com/img/sp_vibe.cc4cc861.svg);
-        background-size: 808px 764px;
+import {
+    List,
+    TrackLeft,
+    TrackMiddle,
+    TrackRight,
+    TrackPlayBtnContainer,
+    TrackTitle,
+    TrackMiddleElem,
+    Mp3,
+    ShowLyricButton,
+    LikeOptions,
+} from './TrackRowCard.styles';
 
-        background-position: -710px -562px;
-        width: 30px;
-        height: 20px;
-        display: inline-block;
-    }
-`;
-const ShowLyricButton = styled.div`
-    &::after {
-        content: '';
-        background-image: linear-gradient(transparent, transparent),
-            url(https://vibe.naver.com/img/sp_vibe.cc4cc861.svg);
-        background-size: 808px 764px;
-
-        background-position: -284px -716px;
-        width: 20px;
-        height: 20px;
-        display: inline-block;
-    }
-`;
-const LikeOptions = styled.div`
-    &::after {
-        content: '';
-        background-image: linear-gradient(transparent, transparent),
-            url(https://vibe.naver.com/img/sp_vibe.cc4cc861.svg);
-        background-size: 808px 764px;
-
-        background-position: -760px -325px;
-        width: 18px;
-        height: 18px;
-        display: inline-block;
-    }
-`;
 const TrackRowCard = ({ trackId, albumImgSrc, trackTitle, artist, albumTitle }) => (
     <List>
         <TrackLeft>
