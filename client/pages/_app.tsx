518757ba766be971fe0480f267db9d3636ce8c1f
mingd1023 <mingd1023@gmail.com>
2020-12-01 22:39:36 +0900

    [FE] Chore: 헤더사이드메뉴를 로그인 한 버전으로 보이도록 수정
    보관함 구현 확인을 위해 유저 더미데이터를 생성하여 로그인한 상태로 변경

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,20 +1,30 @@
 //_app.js에서는 페이지들의 공통되는 부분을 처리
 import PropTypes from 'prop-types';
 import Head from 'next/head';   //head를 수정할 수 있게 함
-import HeaderSideBar from '@components/organisms/HeaderSideBar';
+import styled from 'styled-components';
 
+import HeaderSideBar from '@components/organisms/HeaderSideBar';
 import '../assets/global.css';
+
+const Container = styled.div`
+    background-color: white;
+`;
+
+const userData =  {
+    id: '0',
+    name: 'testUser',
+};
 
 const App = ({ Component, pageProps }) => {
     return (
-        <>
+        <Container>
             <Head>
                 <meta charSet="utf-8" />
                 <title>miniVibe</title>
             </Head>
-            <HeaderSideBar />
+            <HeaderSideBar user = {userData} />
             <Component {...pageProps}/>
-        </>
+        </Container>
     )
 };
 
