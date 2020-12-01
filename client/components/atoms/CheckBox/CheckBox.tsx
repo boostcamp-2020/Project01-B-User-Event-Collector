91665575b0ceed548b863887d82c193479006af2
shany <shany6310@gmail.com>
2020-12-02 03:18:26 +0900

    [FE] feat: 트랙 로우 카드 left side 랜더링  #197
    - 트랙 로우 카드의 왼쪽 부분 랜더링 및 styled-component 적용
    - 컴포넌트 CSS 가이드 라인(border)추가

--- client/components/atoms/CheckBox/CheckBox.tsx
+++ client/components/atoms/CheckBox/CheckBox.tsx
@@ -4,6 +4,21 @@ import styled from 'styled-components';
 interface CheckBoxProps {
     id: string;
 }
-const CheckBox = ({ id }: CheckBoxProps) => <input type="checkbox" id={id} />;
+
+const StyledCheckBox = styled.div`
+    width: 40px;
+    box-sizing: border-box;
+    padding: 10px 12px;
+`;
+const Input = styled.input`
+    width: 16px;
+    height: 16px;
+    margin: 0;
+`;
+const CheckBox = ({ id }: CheckBoxProps) => (
+    <StyledCheckBox>
+        <Input type="checkbox" id={id} />
+    </StyledCheckBox>
+);
 
 export default CheckBox;
--- client/components/organisms/Cards/TrackRowCard/TrackRowCard.tsx
+++ client/components/organisms/Cards/TrackRowCard/TrackRowCard.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
+import A from '@components/atoms/A/A';
 import CheckBox from '@components/atoms/CheckBox/CheckBox';
+import TrackPlayButton from '@components/molecules/TrackPlayButton/TrackPlayButton';
 
-const List = styled.li``;
-const TrackLeft = styled.div``;
-const TrackMiddle = styled.div``;
-const TrackRight = styled.div``;
-
-const TrackRowCard = ({ id }) => (
+const List = styled.li`
+    list-style: none;
+    border: 1px solid black;
+    margin: 0;
+`;
+const TrackLeft = styled.div`
+    border: 1px solid blue;
+    display: flex;
+    height: 50px;
+    padding: 5px 0;
+    box-sizing: border-box;
+`;
+const TrackMiddle = styled.div`
+    border: 1px solid blue;
+`;
+const TrackRight = styled.div`
+    border: 1px solid blue;
+`;
+const TrackPlayBtnContainer = styled.div`
+    padding-right: 7px;
+    height: 40px;
+`;
+const TrackTitle = styled.div`
+    padding: 13px 0 13px 7px;
+`;
+const TrackRowCard = ({ trackId, albumImgSrc, trackTitle }) => (
     <List>
         <TrackLeft>
-            <CheckBox id={id} />
+            <CheckBox id={trackId} />
+            <TrackPlayBtnContainer>
+                <TrackPlayButton src={albumImgSrc} />
+            </TrackPlayBtnContainer>
+            <TrackTitle>
+                <A href="#">{trackTitle}</A>
+            </TrackTitle>
         </TrackLeft>
-        <TrackMiddle></TrackMiddle>
-        <TrackRight></TrackRight>
+        {/* <TrackMiddle></TrackMiddle>
+        <TrackRight></TrackRight> */}
     </List>
 );
 
