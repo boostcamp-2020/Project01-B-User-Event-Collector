dbfd08c23e66830e63e8d1592ccd5a651f486bbf
shany <shany6310@gmail.com>
2020-12-02 04:48:46 +0900

    [FE] test: 트랙 로우 카드 컴포넌트 샘플 데이터 생성 #197
    - 트랙 로우 카드 컴포넌트 테스트를 위한 샘플 데이터 생성

--- client/components/organisms/Cards/TrackRowCard/TrackRowCard.stories.tsx
+++ client/components/organisms/Cards/TrackRowCard/TrackRowCard.stories.tsx
@@ -5,5 +5,11 @@ export default {
     title: 'TrackRowCard',
     component: TrackRowCard,
 };
-
-export const Default = () => <TrackRowCard />;
+const TrackRowCardData = {
+    albumImgSrc: 'https://musicmeta-phinf.pstatic.net/artist/002/826/2826154.jpg?type=ff300_300&v=20191231151906',
+    trackId: '1',
+    trackTitle: 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)',
+    artist: '미란이',
+    albumTitle: '쇼미더머니 9 Episode 1',
+};
+export const Default = () => <TrackRowCard {...TrackRowCardData} />;
