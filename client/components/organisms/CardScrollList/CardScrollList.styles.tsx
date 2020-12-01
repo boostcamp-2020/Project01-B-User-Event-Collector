dc66c68d673f8ec41ff7557115ffa0592aceb8ed
YimJiYoung <chiy323@gmail.com>
2020-12-01 11:38:41 +0900

    [FE] Refactor: CardScrollList type props 제거, chileren 받도록 수정
    - 재사용성을 위해 type이 아닌 children으로 각 컨텐츠 리스트 받도록 수정

--- client/components/organisms/CardScrollList/CardScrollList.styles.tsx
+++ client/components/organisms/CardScrollList/CardScrollList.styles.tsx
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
-const ListContainer = styled.div`
+const Container = styled.div`
     position: relative;
     min-height: 375px;
 `;
 
-const List = styled.ul`
-    display: flex;
-    margin: 0;
-    padding: 0;
+const ListContainer = styled.div`
     overflow-x: auto;
     -ms-overflow-style: none; /* IE */
     scrollbar-width: none; /* Firefox */
@@ -16,15 +13,6 @@ const List = styled.ul`
         display: none !important;  /* 크롬 등  */
       }
     scroll-behavior: smooth;
-`;
-
-const ItemContainer = styled.li`
-    list-style: none;   
-    margin-left: 17px;
-
-    &:first-child {
-        margin-left: 0;
-    }
 `;
 
 const PrevButtonContainer = styled.div` 
@@ -39,4 +27,4 @@ const NextButtonContainer = styled.div`
     right: -27px;
 `
 
-export { List, ListContainer, ItemContainer, PrevButtonContainer, NextButtonContainer };
+export { Container, ListContainer, PrevButtonContainer, NextButtonContainer };
