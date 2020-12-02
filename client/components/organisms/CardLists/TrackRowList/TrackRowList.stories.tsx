40658d20fb690b9b9d30cc25ce72733bf482ee22
shany <shany6310@gmail.com>
2020-12-02 14:48:14 +0900

    [FE] test: 트랙 로우 카드 리스트 storybook 코드 작성 #203
    - 트랙 로우 카드 리스트 컴포넌트 테스트를 위한 storybook 코드 작성

--- client/components/organisms/CardLists/TrackRowList/TrackRowList.stories.tsx
+++ client/components/organisms/CardLists/TrackRowList/TrackRowList.stories.tsx
@@ -5,5 +5,11 @@ export default {
     title: 'TrackRowList',
     component: TrackRowList,
 };
-
-export const Default = () => <TrackRowList />;
+const TrackDatas = Array(20).fill({
+    albumImgSrc: 'https://musicmeta-phinf.pstatic.net/artist/002/826/2826154.jpg?type=ff300_300&v=20191231151906',
+    trackId: '1',
+    trackTitle: 'VVS (Feat. JUSTHIS) (Prod. GroovyRoom)',
+    artist: '미란이',
+    albumTitle: '쇼미더머니 9 Episode 1',
+});
+export const Default = () => <TrackRowList items={TrackDatas} />;
