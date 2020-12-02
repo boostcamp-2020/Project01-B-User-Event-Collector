0b2272712fa1d753861f4e21909204fed6fee0f6
shany <shany6310@gmail.com>
2020-12-02 18:31:45 +0900

    [FE] feat: A 태그 스타일 추가 #149
    - TrackInfo에 사용하기 위한 스타일 추가

--- client/components/atoms/A/A.styles.tsx
+++ client/components/atoms/A/A.styles.tsx
@@ -1,33 +1,36 @@
 import styled from 'styled-components';
 
 interface StyledAProps {
-  variant?: 'primary' | 'secondary' | 'tertiary';
+    variant?: 'primary' | 'secondary' | 'tertiary' | 'fourth';
 }
 
 const getAStyle = (props: StyledAProps) => {
-  let color = 'black';
-  let fontWeight = '400';
-  let fontSize;
+    let color = 'black';
+    let fontWeight = '400';
+    let fontSize;
 
-  switch (props.variant) {
-    case 'primary':
-      fontWeight = '800';
-      fontSize = '26px';
-      break;
-    case 'secondary':
-      fontWeight = '700';
-      fontSize = '17px';
-      break;
-    case 'tertiary':
-      color = '#999';
-      fontSize = '13px';
-      break;
-    default:
-      fontSize = '16px';
-      break;
-  }
+    switch (props.variant) {
+        case 'primary':
+            fontWeight = '800';
+            fontSize = '26px';
+            break;
+        case 'secondary':
+            fontWeight = '700';
+            fontSize = '17px';
+            break;
+        case 'tertiary':
+            color = '#999';
+            fontSize = '13px';
+            break;
+        case 'fourth':
+            fontSize = '14px';
+            break;
+        default:
+            fontSize = '16px';
+            break;
+    }
 
-  return `
+    return `
     color: ${color};
     font-size:  ${fontSize};
     font-weight: ${fontWeight};
@@ -40,7 +43,7 @@ const getAStyle = (props: StyledAProps) => {
 };
 
 export const StyledA = styled.a<StyledAProps>`
-${(props) => getAStyle(props)};
+    ${(props) => getAStyle(props)};
 `;
 
 export default StyledA;
