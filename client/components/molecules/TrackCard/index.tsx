e79153549a8ee8d4c1a863ca1f4c880d0bc552ba
shany <shany6310@gmail.com>
2020-12-02 18:54:26 +0900

    [FE] feat: 트랙카드 컴포넌트 구현 #149
    - 플레이버튼, 노래제목, 아티스트 정보가 있는 트랙카드 컴포넌트 구현 및 styled-components 적용

---
+++ client/components/molecules/TrackCard/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import TrackPlayButton from '@components/molecules/TrackPlayButton';
+import TrackInfo from '@components/molecules/TrackInfo';
+
+const TrackCardContainer = styled.div<{ isDefault: boolean }>`
+    border: 1px solid black;
+    display: flex;
+    height: 46px;
+    ${(props) =>
+        props.isDefault &&
+        css`
+            padding: 8px 0;
+            height: 40px;
+        `}
+`;
+const TrackCard = ({ src, trackTitle, artist, imgVariant, isDefault, isTrack }) => (
+    <TrackCardContainer isDefault={isDefault}>
+        <TrackPlayButton src={src} imgVariant={imgVariant} />
+        <TrackInfo trackTitle={trackTitle} artist={artist} track={isTrack} />
+    </TrackCardContainer>
+);
+
+export default TrackCard;
