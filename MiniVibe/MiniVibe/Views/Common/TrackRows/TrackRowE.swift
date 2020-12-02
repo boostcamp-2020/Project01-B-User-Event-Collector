705dbff968e1726bd665dcb45bf5cb24013ceda3
Sue Cho <suejeong97@naver.com>
2020-12-03 03:38:24 +0900

    [iOS] Feat: Track Row 종류 추가 #210
    - 차트 뷰에서 사용되는 track row 추가

---
+++ MiniVibe/MiniVibe/Views/Common/TrackRows/TrackRowE.swift
@@ -0,0 +1,48 @@
+//
+//  TrackRowE.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/12/03.
+//
+
+import SwiftUI
+
+struct TrackRowE: View {
+    let order: Int
+    let title: String
+    let artist: String
+
+    var body: some View {
+        HStack {
+            NavigationLink(destination: AlbumPlaylistView(title: title, subtitle: artist)) {
+                Image("album")
+                    .trackRowImageConfigure()
+            }
+            
+            HStack(alignment: .top) {
+                Text("\(order)")
+                    .font(.title3)
+                    .padding(.horizontal, 4)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(title)
+                        .font(.title3)
+                    
+                    Text(artist)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .scaledToFit()
+            
+            Spacer()
+        }
+    }
+}
+
+struct TrackRowE_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackRowE(order: 3, title: "Dynamite", artist: "방탄소년단")
+            .previewLayout(.fixed(width: 375, height: 80))
+    }
+}
