342824e92323d4e781e7d768e496da0e921e8967
YimJiYoung <chiy323@gmail.com>
2020-12-02 23:30:42 +0900

    [FE] Fix: material ui classname mismatch 문제 해결
    - material ui를 사용한 progressBar 컴포넌트에서 server와 client의 classname이
    매칭되지 않는 오류 발생
    - https://github.com/mui-org/material-ui/tree/master/examples/nextjs/pages 참>고하여 해결
    - material ui에서 classname을 동적으로 생성하는데 server side와 client side에>서 생성한 classname이 매칭되지 않아 발생하는 문제로 추정 -> server side에서 만
    들어진 style 삭제

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,36 +1,46 @@
 //_app.js에서는 페이지들의 공통되는 부분을 처리
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Head from 'next/head';   //head를 수정할 수 있게 함
+import Head from 'next/head'; //head를 수정할 수 있게 함
 import styled from 'styled-components';
 
 import HeaderSideBar from '@components/organisms/HeaderSideBar';
+import PlayController from '@components/organisms/PlayController';
 import '../assets/global.css';
 
 const Container = styled.div`
     background-color: white;
 `;
 
-const userData =  {
+const userData = {
     id: '0',
     name: 'testUser',
 };
 
 const App = ({ Component, pageProps }) => {
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
     return (
         <Container>
             <Head>
                 <meta charSet="utf-8" />
                 <title>miniVibe</title>
             </Head>
-            <HeaderSideBar user = {userData} />
-            <Component {...pageProps}/>
+            <HeaderSideBar user={userData} />
+            <PlayController />
+            <Component {...pageProps} />
         </Container>
-    )
+    );
 };
 
 App.propTypes = {
     Component: PropTypes.elementType.isRequired,
-}
+};
 
 export default App;
-
