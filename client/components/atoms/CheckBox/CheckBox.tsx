65f7da7719985ef7a81763b3f6461214061700e3
shany <shany6310@gmail.com>
2020-12-04 01:53:30 +0900

    [FE] fix: CheckBox Style 수정 #66
    - 체크박스 클릭시 바이브웹과 같도록 background-image 설정 추가
    - label 과 연결하여 checkbox 클릭시 label의 스타일이 변경되도록 함

--- client/components/atoms/CheckBox/CheckBox.tsx
+++ client/components/atoms/CheckBox/CheckBox.tsx
@@ -7,17 +7,34 @@ interface CheckBoxProps {
 
 const StyledCheckBox = styled.div`
     width: 40px;
-    box-sizing: border-box;
     padding: 10px 12px;
+    position: relative;
 `;
 const Input = styled.input`
-    width: 16px;
-    height: 16px;
-    margin: 0;
+    display: none;
+
+    & + label {
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        position: absolute;
+        border: 1px solid #0000001f;
+        top: 10px;
+        left: 12px;
+    }
+    &:checked + label {
+        background-image: linear-gradient(transparent, transparent),
+            url(https://vibe.naver.com/img/sp_vibe.cc4cc861.svg);
+        background-size: 808px 764px;
+        background-position: -440px -744px;
+        border: none;
+    }
 `;
+
 const CheckBox = ({ id }: CheckBoxProps) => (
     <StyledCheckBox>
         <Input type="checkbox" id={id} />
+        <label htmlFor={id} />
     </StyledCheckBox>
 );
 
