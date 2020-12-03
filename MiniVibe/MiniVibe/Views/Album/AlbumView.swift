ddebd04fe5854c15822bcebe60e3fc49953fcec7
TTOzzi <devttozzi@gmail.com>
2020-12-04 01:49:18 +0900

    [iOS] Feat: Article 화면전환 구현 #135
    앨범, 플레이리스트 화면에서 썸네일 이미지, 더보기를 눌렀을 때 Article 화면을 표시하도록 구현
    isOpenArticle이라는 상태값을 두고 if문을 활용해 표시할 뷰를 랜더링하도록 구현함

--- MiniVibe/MiniVibe/Views/Album/AlbumView.swift
+++ MiniVibe/MiniVibe/Views/Album/AlbumView.swift
@@ -15,65 +15,73 @@ struct AlbumView: View {
     
     @State private var activeSheet: ActiveSheet = .album
     @State private var showSheet = false
+    @State private var isOpenArticle = false
     let title: String
     let subtitle: String
     
     var body: some View {
         GeometryReader { geometry in
             let width: CGFloat = geometry.size.width
-            
-            ScrollView {
-                VStack(spacing: 36) {
-                    VStack {
-                        PlaylistAlbumInfo(title: title, subtitle: subtitle)
-                            .padding(.vertical, 10)
-                        
-                        LazyVGrid(
-                            columns: [.init(.fixed(geometry.size.width))],
-                            pinnedViews: [.sectionHeaders]
-                        ) {
-                            Section(header: PlayAndShuffle(width: geometry.size.width)) {
-                                ForEach(0..<7) { index in
-                                    TrackRowD(order: index + 1,
-                                              title: "Dynamite",
-                                              artist: "방탄소년단"
-                                    ) {
-                                        activeSheet = .track
-                                        showSheet = true
+            if isOpenArticle {
+                Article(isOpenArticle: $isOpenArticle,
+                        title: title,
+                        subtitle: subtitle)
+            } else {
+                ScrollView {
+                    VStack(spacing: 36) {
+                        VStack {
+                            PlaylistAlbumInfo(title: title,
+                                              subtitle: subtitle,
+                                              isOpenArticle: $isOpenArticle)
+                                .padding(.vertical, 10)
+                            
+                            LazyVGrid(
+                                columns: [.init(.fixed(geometry.size.width))],
+                                pinnedViews: [.sectionHeaders]
+                            ) {
+                                Section(header: PlayAndShuffle(width: geometry.size.width)) {
+                                    ForEach(0..<7) { index in
+                                        TrackRowD(order: index + 1,
+                                                  title: "Dynamite",
+                                                  artist: "방탄소년단"
+                                        ) {
+                                            activeSheet = .track
+                                            showSheet = true
+                                        }
                                     }
                                 }
+                                .padding(.horizontal, geometry.size.width * .paddingRatio)
                             }
-                            .padding(.horizontal, geometry.size.width * .paddingRatio)
                         }
+                        
+                        AlbumSection(
+                            width: width,
+                            destination: ThumbnailGridView(title: "아티스트의 다른 앨범"),
+                            title: "아티스트의 다른 앨범"
+                        )
+                        
+                        ArtistSection(width: width,
+                                      sectionTitle: "비슷한 아티스트")
+                        
+                        PlayListSection(
+                            width: width,
+                            title: "관련 플레이리스트",
+                            destination: ThumbnailList(title: "관련 플레이리스트",
+                                                       info: .playlist)
+                        )
                     }
-                    
-                    AlbumSection(
-                        width: width,
-                        destination: ThumbnailGridView(title: "아티스트의 다른 앨범"),
-                        title: "아티스트의 다른 앨범"
-                    )
-                    
-                    ArtistSection(width: width,
-                                  sectionTitle: "비슷한 아티스트")
-                    
-                    PlayListSection(
-                        width: width,
-                        title: "관련 플레이리스트",
-                        destination: ThumbnailList(title: "관련 플레이리스트",
-                                                   info: .playlist)
-                    )
+                    .padding(.bottom, 70)
                 }
-            }
-            .padding(.bottom, 70)
-            .navigationBarTitleDisplayMode(.inline)
-            .navigationBarItems(
-                trailing: trailingBarButtons
-            )
-            .fullScreenCover(isPresented: $showSheet) {
-                if activeSheet == .album {
-                    AlbumMenu(title: title, subtitle: subtitle)
-                } else {
-                    PlayerMenu(title: title, subtitle: subtitle)
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarItems(
+                    trailing: trailingBarButtons
+                )
+                .fullScreenCover(isPresented: $showSheet) {
+                    if activeSheet == .album {
+                        AlbumMenu(title: title, subtitle: subtitle)
+                    } else {
+                        PlayerMenu(title: title, subtitle: subtitle)
+                    }
                 }
             }
         }
