b33db1b8bcc480482e62c96ff11ad03791917557
TTOzzi <devttozzi@gmail.com>
2020-12-04 10:25:43 +0900

    [iOS] Fix: 검색 화면, 보관함 화면에서 재생화면을 통한 화면전환이 되지 않는 문제 #265
    - 검색 화면에 NavigationLink 생성
    - 보관함 화면에 NavigationLink 생성
    - 검색, 보관함 화면에 nowPlaying environmentObject 선언

--- MiniVibe/MiniVibe/Search.swift
+++ MiniVibe/MiniVibe/Search.swift
@@ -8,28 +8,35 @@
 import SwiftUI
 
 struct Search: View {
+    @EnvironmentObject var nowPlaying: NowPlaying
     @State var searchedText = ""
     
     var body: some View {
         GeometryReader { geometry in
             let width = geometry.size.width
-            
-            VStack(alignment: .leading) {
-                Text("검색")
-                    .foregroundColor(.black)
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .padding(geometry.size.width * .paddingRatio)
-                
-                SearchBar(searchedText: $searchedText, width: width)
-                
-                NewsSection(width: width)
-                
-                GenreSection()
+            NavigationView {
+                VStack(alignment: .leading) {
+                    NavigationLink(
+                        destination: nowPlaying.destination?.view,
+                        isActive: $nowPlaying.isNavigationActive) { }
+                    
+                    Text("검색")
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .padding(geometry.size.width * .paddingRatio)
+                    
+                    SearchBar(searchedText: $searchedText, width: width)
+                    
+                    NewsSection(width: width)
+                    
+                    GenreSection()
+                }
             }
+            .navigationBarHidden(true)
         }
     }
-
+    
 }
 
 struct Genre_Previews: PreviewProvider {
