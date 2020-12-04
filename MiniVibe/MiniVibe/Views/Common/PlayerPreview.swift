b5d1253a61ced00725c0f966f22b6fa3a1f04295
Sue Cho <suejeong97@naver.com>
2020-12-04 11:08:34 +0900

    [iOS] Fix: 재생바 UI 수정 #267
    - 재생 프리뷰의 색상 변경
    - 쉼표 선택 시 플레이어 창 뜨기 수정

--- MiniVibe/MiniVibe/Views/Common/PlayerPreview.swift
+++ MiniVibe/MiniVibe/Views/Common/PlayerPreview.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct PlayerPreview: View {
     @State private var isPlaying = false
+    @EnvironmentObject var nowPlaying: NowPlaying
+    
     let coordinate: CGRect
     let title: String
     let artist: String
@@ -16,52 +18,64 @@ struct PlayerPreview: View {
     private let edgeInset = EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)
     
     var body: some View {
-        HStack {
-            Image("album")
-                .resizable()
-                .aspectRatio(1, contentMode: .fit)
-                .frame(height: height)
-            
-            VStack(alignment: .leading) {
-                Text(title)
-                    .font(.system(size: 15))
+        VStack {
+            HStack {
+                albumInfo
                 
-                Text(artist)
-                    .font(.system(size: 12))
-                    .foregroundColor(.secondary)
+                Spacer()
+                
+                controlIcons
+                    .font(.system(size: 20))
+                    .foregroundColor(.black)
             }
-            
-            Spacer()
-            
-            HStack(spacing: 10) {
-                Button {
-                    isPlaying.toggle()
-                } label: {
-                    isPlaying ? Image(systemName: "pause.fill") : Image(systemName: "play.fill")
-                }
-                
-                Button {
-                    
-                } label: {
-                    Image(systemName: "forward.fill")
-                }
-                
-                Button {
-                    
-                } label: {
-                    Image(systemName: "music.note.list")
-                }
-            }
-            .font(.system(size: 20))
-            .foregroundColor(.black)
+            .frame(height: height)
+            .padding(edgeInset)
+            .background(Color(.systemGray6).opacity(0.99))
+            .position(
+                x: coordinate.width / 2,
+                y: coordinate.height - (height / 2 + edgeInset.bottom)
+            )
         }
-        .frame(height: height)
-        .padding(edgeInset)
-        .background(Color.gray.opacity(0.9))
-        .position(
-            x: coordinate.width / 2,
-            y: coordinate.height - (height / 2 + edgeInset.bottom)
-        )
+        
+    }
+    
+    @ViewBuilder
+    var albumInfo: some View {
+        Image("album")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .frame(height: height)
+        
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.system(size: 15))
+            
+            Text(artist)
+                .font(.system(size: 12))
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    var controlIcons: some View {
+        HStack(spacing: 10) {
+            Button {
+                isPlaying.toggle()
+            } label: {
+                isPlaying ? Image(systemName: "pause.fill") : Image(systemName: "play.fill")
+            }
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "forward.fill")
+            }
+            
+            Button {
+                nowPlaying.isPlayerOpen.toggle()
+            } label: {
+                Image(systemName: "music.note.list")
+            }
+        }
     }
 }
 
