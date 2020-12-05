5d59d112b3c1ebbb7c43c6888755e3b08e439395
YimJiYoung <chiy323@gmail.com>
2020-12-05 12:43:07 +0900

    [BE] Feat: library_trakcs/albums/artists/playlists 관계  테이블 정의 #245
    - ManyToMany, JoinTable 데코레이터를 이용하여 다대다 관계 테이블 정의

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,7 +1,10 @@
 import {
-    Entity, PrimaryGeneratedColumn, Column, OneToMany,
+    Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToMany, JoinTable,
 } from 'typeorm';
 import Playlist from './Playlist';
+import Track from './Track';
+import Album from './Album';
+import Artist from './Artist';
 
 @Entity()
 class User {
@@ -22,16 +25,22 @@ class User {
 
     @OneToMany((type) => Playlist, (playlist) => playlist.user)
     playlists: Playlist[];
+
+    @ManyToMany(() => Track)
+    @JoinTable({ name: 'library_tracks' })
+    libraryTracks: Track[];
+
+    @ManyToMany(() => Album)
+    @JoinTable({ name: 'library_albums' })
+    libraryAlbums: Album[];
+
+    @ManyToMany(() => Artist)
+    @JoinTable({ name: 'library_artists' })
+    libraryArtists: Track[];
+
+    @ManyToMany(() => Playlist)
+    @JoinTable({ name: 'library_playlists' })
+    libraryPlaylists: Playlist[];
 }
 
 export default User;
-
-// import mongoose, { mongo } from 'mongoose';
-
-// const userSchema = new mongoose.Schema({
-//     id: {type: String, required: true, unique: true},
-//     pw: {type: String, required: true, trim: true},
-// });
-// userSchema.index({id: 1});
-
-// export default mongoose.model('User', userSchema);
