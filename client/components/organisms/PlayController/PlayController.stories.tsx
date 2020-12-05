8f1ca00b6467bf12d220f581e08f38fe2ffd31ca
mingd1023 <mingd1023@gmail.com>
2020-12-06 01:44:35 +0900

    [FE] Refactor: 뮤직 플레이어 관련 컴포넌트들을 MusicPlayer 폴더로 이동
    PlayerController, PlayerTrackInfo 이동

--- client/components/organisms/PlayController/PlayController.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import PlayController from './index';
-
-export default {
-    title: 'Organisms/PlayController',
-    component: PlayController,
-};
-
-export const Default = () => <PlayController />;
