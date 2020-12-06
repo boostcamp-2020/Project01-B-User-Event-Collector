15421e61b850a321643317cb2802fd6a403b7738
YimJiYoung <chiy323@gmail.com>
2020-12-06 21:07:20 +0900

    [BE] Feat: 아티스트 전체 조회/개별 조회 API 구현  #291 #273
    - 아티스트 전체 조회 API 구현
    - 아티스트 개별 조회 API 구현

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import artistRouter from './artists';
 import newsRouter from './news';
 import magazineRouter from './magazines';
 import libraryRouter from './library';
 
-
 const router = express.Router();
 
+router.use('/artists', artistRouter);
 router.use('/news', newsRouter);
 router.use('/magazines', magazineRouter);
 router.use('/library', libraryRouter);
