d49cfb5256c37d777a8c1878aa4f8d34c6c5ec69
YimJiYoung <37496919+YimJiYoung@users.noreply.github.com>
2020-12-06 19:21:08 +0900

    Revert "[BE] 보관함 트랙 관련 API 구현"

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import newsRouter from './news';
-import libraryRouter from './library';
 
 const router = express.Router();
 
 router.use('/news', newsRouter);
-router.use('/library', libraryRouter);
 
 export default router;
--- server/src/routes/library/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import trackRouter from './tracks';
-
-const router = express.Router();
-
-router.use('/tracks', trackRouter);
-
-export default router;
