11a156fd3d0cec584e61c74747ee9fbf1fc185c6
mingd1023 <mingd1023@gmail.com>
2020-12-06 19:04:21 +0900

    [FE] Test: 플로팅 셀렉트 메뉴 스토리 작성

---
+++ client/components/organisms/FloatingSelectMenu/FloatingSelectMenu.stories.tsx
@@ -0,0 +1,8 @@
+import FloatingSelectMenu from './index';
+
+export default {
+    title: 'Organisms/FloatingSelectMenu',
+    component: FloatingSelectMenu,
+};
+
+export const Default = () => <FloatingSelectMenu />
