aa142c8dee919545fa6eab3cbf69bdd8db3fc9ef
shany <shany6310@gmail.com>
2020-12-07 05:52:23 +0900

    [BE] feat: 유저 보관함 - 플레이리스트 리스트 조회 api #295
    - 유저 플레이리스트 보관함에 있는 플레이리스트들을 조회하는 api 구현

--- server/src/routes/library/playlists/index.ts
+++ server/src/routes/library/playlists/index.ts
@@ -3,6 +3,6 @@ import * as playlistsController from './library.playlists.controller';
 
 const router = express.Router();
 
+router.get('/', playlistsController.list);
 router.post('/', playlistsController.create);
-
 export default router;
--- server/src/routes/library/playlists/library.playlists.controller.ts
+++ server/src/routes/library/playlists/library.playlists.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import { getManager } from 'typeorm';
+import { getManager, getRepository } from 'typeorm';
 import Playlist from '../../../models/Playlist';
 import User from '../../../models/User';
 
@@ -23,4 +23,28 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
     }
 };
 
-export { create };
+const list = async (req: Request, res: Response, next: NextFunction) => {
+    const userId = 2;
+
+    try {
+        const UserRepository = getRepository(User);
+        const user = await UserRepository.createQueryBuilder('user')
+            .leftJoinAndSelect('user.libraryPlaylists', 'library_playlists')
+            .select([
+                'user.id',
+                'library_playlists.id',
+                'library_playlists.title',
+                'library_playlists.subTitle',
+                'library_playlists.imageUrl',
+            ])
+            .where('user.id = :id', { id: userId })
+            .getOne();
+
+        const libraryPlaylists = user?.libraryPlaylists || [];
+        return res.json({ success: true, data: libraryPlaylists });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false });
+    }
+};
+export { create, list };
