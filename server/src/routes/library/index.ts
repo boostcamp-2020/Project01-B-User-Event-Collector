c9656c6574304230bad4ac04ea42307e13c99d37
mingd1023 <mingd1023@gmail.com>
2020-12-07 00:15:05 +0900

    [BE] Feat: 앨범 라우터와 아티스트 라우터 추가

--- server/src/routes/library/index.ts
+++ server/src/routes/library/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import trackRouter from './tracks';
+import albumRouter from './album';
+import artistRouter from './artist';
 
 const router = express.Router();
 
 router.use('/tracks', trackRouter);
+router.use('/album', albumRouter);
+router.use('/artist', artistRouter);
 
 export default router;
