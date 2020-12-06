9ae9c737d1c39c74f5b04d2e2426bead68d2a285
YimJiYoung <chiy323@gmail.com>
2020-12-06 18:42:23 +0900

    [BE] Feat: 보관함 트랙 리스트 조회  api 구현 #285
        - GET /library/tracks api 구현

--- server/src/routes/library/tracks/index.ts
+++ server/src/routes/library/tracks/index.ts
@@ -3,6 +3,7 @@ import * as trackController from './library.tracks.controller';
 
 const router = express.Router();
 
+router.get('/', trackController.list);
 router.post('/', trackController.create);
 
 export default router;
--- server/src/routes/library/tracks/library.tracks.controller.ts
+++ server/src/routes/library/tracks/library.tracks.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { getManager } from 'typeorm';
+import { getManager, getRepository } from 'typeorm';
 import User from '../../../models/User';
 import Track from '../../../models/Track';
 
@@ -24,4 +24,33 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export { create };
+const list = async (req: Request, res: Response, next: NextFunction) => {
+    // TODO: 인증 구현 후 수정
+    const userId = 1;
+
+    const userRepository = getRepository(User);
+    // const libraryTracks = await userRepository.findOne(userId, { relations: ['libraryTracks'] });
+    const libraryTracks = await userRepository.createQueryBuilder('user')
+        .leftJoinAndSelect('user.libraryTracks', 'library_tracks')
+        .leftJoinAndSelect('library_tracks.album', 'album')
+        .leftJoinAndSelect('library_tracks.artist', 'artist')
+        .select([
+            'user.id',
+            'library_tracks.id',
+            'library_tracks.title',
+            'library_tracks.lyrics',
+            'artist.id',
+            'artist.name',
+            'album.id',
+            'album.title',
+            'album.imageUrl',
+        ])
+        .getOne();
+
+    res.json({
+        success: true,
+        data: libraryTracks,
+    });
+};
+
+export { create, list };
