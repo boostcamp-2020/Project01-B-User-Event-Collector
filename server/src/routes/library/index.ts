ed7446443a4909acdd189da4ef28df796694e776
shany <shany6310@gmail.com>
2020-12-07 16:57:29 +0900

    [BE] feat: 유저 보관함 - 믹스테잎 리스트 조회 api #318
    - 유저 보관함에 있는 믹스테잎 리스트를 조회하는 api 구현

--- server/src/routes/library/index.ts
+++ server/src/routes/library/index.ts
@@ -3,7 +3,7 @@ import trackRouter from './tracks';
 import playlistsRouter from './playlists';
 import albumRouter from './album';
 import artistRouter from './artist';
-
+import mixtapeRouter from './mixtapes';
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.use('/tracks', trackRouter);
 router.use('/playlists', playlistsRouter);
 router.use('/album', albumRouter);
 router.use('/artist', artistRouter);
+router.use('/mixtapes', mixtapeRouter);
 
 export default router;
