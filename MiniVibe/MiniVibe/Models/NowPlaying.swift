f1d05f5fbea840b359240b71184f628fb6efc18e
Sue Cho <suejeong97@naver.com>
2020-12-08 22:54:21 +0900

    [iOS] Feat: NowPlaying 구성하기 #345
    - NowPlaying class 생성
    - environment object로 주입

---
+++ MiniVibe/MiniVibe/Models/NowPlaying.swift
@@ -0,0 +1,14 @@
+//
+//  NowPlaying.swift
+//  MiniVibe
+//
+//  Created by Sue Cho on 2020/12/08.
+//
+
+import Foundation
+
+class NowPlaying: ObservableObject {
+    @Published var isPlaying: Bool = false
+    @Published var isPlayerPresented: Bool = false
+    @Published var upNext = [Track]()
+}
