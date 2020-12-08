452738a6d8eb9cdce02ea03a97fd5ac6cf8f7da8
YimJiYoung <chiy323@gmail.com>
2020-12-08 18:11:08 +0900

    [BE] Feat: 플레이리스트 상세 조회 시 연관된 아티스트 목록 추가
    - 플레이리스트 상세 조회 시 연관된 아티스트 목록도 함께 보내주도록 수정

--- server/src/routes/playlists/playlists.controller.ts
+++ server/src/routes/playlists/playlists.controller.ts
@@ -4,6 +4,7 @@ import * as libraryPlaylist from '../library/playlists/library.playlists.control
 import * as TrackController from '../tracks/tracks.controller';
 import Playlist from '../../models/Playlist';
 import Track from '../../models/Track';
+import Artist from '../../models/Artist';
 
 const list = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -46,7 +47,14 @@ const listById = async (req: Request, res: Response, next: NextFunction) => {
             .where('playlist.id = :id', { id })
             .getOne();
 
-        return res.json({ success: true, data: playlist });
+        const relatedArtistIds = [...new Set(playlist?.tracks.map((track) => track.artist.id))];
+
+        const ArtistRepository = getRepository(Artist);
+        const relatedArtists = await ArtistRepository.createQueryBuilder('artist')
+            .where('artist.id IN (:relatedArtistIds)', { relatedArtistIds })
+            .getMany();
+
+        return res.json({ success: true, data: { ...playlist, relatedArtists } });
     } catch (err) {
         console.error(err);
         return res.status(500).json({ success: false, data: [] });
