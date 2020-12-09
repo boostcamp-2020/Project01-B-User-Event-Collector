d2f6f1b62b01c8c6b3fb049bc4673b4cb66395a0
YimJiYoung <chiy323@gmail.com>
2020-12-09 20:08:41 +0900

    [FE] Feat: component info context 정의 및 wrapper 컴포넌트 구현 #358
    - component id/ 가지고 있는 data를 하위 컴포넌트에 전달하기 위한 ComponentInfoContext 정의
    - compoentId를 더하고, data를 추가하여 업데이트한 value를 쉽게 전달하기 위한 ComponentInfoWrapper 컴포넌트 구현

---
+++ client/utils/context/ComponentInfoWrapper.tsx
@@ -0,0 +1,22 @@
+import { useContext, ReactNode } from 'react';
+import ComponentInfoContext from './ComponentInfoContext';
+
+interface IProps {
+    children: ReactNode;
+    componentId: string;
+    data?: {
+        type: string;
+        id: number;
+    };
+}
+
+const ComponentInfoWrapper = ({ children, componentId, data }: IProps) => {
+    const componentInfo = useContext(ComponentInfoContext);
+    componentInfo.componentId += `_${componentId}`;
+
+    return (
+        <ComponentInfoContext.Provider value={{ ...componentInfo, ...data }}>{children}</ComponentInfoContext.Provider>
+    );
+};
+
+export default ComponentInfoWrapper;
