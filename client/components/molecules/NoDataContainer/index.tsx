31dd2f72c35a82a7a746ce0de5f0118911dd913c
shany <shany6310@gmail.com>
2020-12-09 19:30:03 +0900

    [FE] feat: type 에 따른 텍스트 메세지 생성 #357
    - 보관함 타입에 따라 텍스트 메세지를 다르게 보여주기 위해 변수로 메세지 저장

--- client/components/molecules/NoDataContainer/index.tsx
+++ client/components/molecules/NoDataContainer/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import StyledText from '@components/atoms/Text/Text.styles';
+
 const Container = styled.div`
     height: 100%;
     width: 100%;
@@ -17,11 +18,25 @@ const Title = styled(StyledText)`
 const Description = styled(StyledText)`
     font-size: 17px;
 `;
-const NoDataContainer = () => (
+const titleText = {
+    album: '보관한 앨범',
+    artist: '좋아하는 아티스트',
+    playlist: '보관한 플레이리스트',
+    track: '좋아하는 노래',
+    mixtape: '오늘 들을 믹스테잎',
+};
+const descriptionText = {
+    album: '내 마음에 드는 앨범을 모아보세요',
+    artist: '내가 좋아하는 아티스트를 보관함에서 확인해보세요.',
+    playlist: '자주 듣고 싶은 플레이리스트는 보관함에 모아보세요.',
+    track: '내가 좋아하는 노래들을 모아서 감상해보세요.',
+    mixtape: '오늘 들을 믹스테잎이 없습니다.',
+};
+const NoDataContainer = ({ type }) => (
     <Container>
         <TextContainer>
-            <Title>보관한 앨범</Title>
-            <Description>내 마음에 드는 앨범을 모아보세요</Description>
+            <Title>{titleText[type]}</Title>
+            <Description>{descriptionText[type]}</Description>
         </TextContainer>
     </Container>
 );
