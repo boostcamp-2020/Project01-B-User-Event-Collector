5e3d6940a1b888a362111c2b7fe73a26779ced3a
shany <shany6310@gmail.com>
2020-12-11 06:54:53 +0900

    [FE] feat: 드롭다운 메뉴 종류에 대해 상수로 생성 #369
    - 드롭다운 메뉴에 따라 동작을 달리 하기 위해 메뉴이름으로 분기
    - 추후 확장성과 재사용성을 위해 메뉴 종류를 상수로 관리

---
+++ client/constants/dropDownMenu.ts
@@ -0,0 +1,13 @@
+
+const dropDownMenu = {
+    like:'좋아요',
+    unlike:'좋아요 취소',
+    addToPlaylist:'내 플레이리스트에 추가',
+    addToLibrary:'보관함에 추가',
+    addToUpNext:'현재 재생목록에 추가',
+    showLyric:'가사보기',
+    buyMP3:'MP3 구매',
+    share:'공유'
+}
+
+export default dropDownMenu;
