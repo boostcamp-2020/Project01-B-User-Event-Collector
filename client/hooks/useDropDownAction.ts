ff3d91e981052a01cd5fb8c0a53e1d4ca317572b
shany <shany6310@gmail.com>
2020-12-11 07:25:30 +0900

    [FE] feat: 드롭다운메뉴액션 훅에서 가사보기, 내 플레이리스트에 추가 동작 구현 #369
    - 드롭다운메뉴 액션으로 가사보기, 내플레이리스트에 추가 동작 구현
    - 두 동작모두 모달 창을 띄움
    - 내 플레이리스트에 추가는 유저 보관함의 플레이리스트 리스트를 가져옴

--- client/hooks/useDropDownAction.ts
+++ client/hooks/useDropDownAction.ts
@@ -1,8 +1,8 @@
 import { useContext } from 'react';
 import ComponentInfoContext from '@utils/context/ComponentInfoContext';
 import dropDownMenu from '@constants/dropDownMenu';
-import {likeRequest, unlikeRequest} from '@utils/apis'
-import apiUrl from '@constants/apiUrl'
+import { likeRequest, unlikeRequest, request } from '@utils/apis';
+import apiUrl from '@constants/apiUrl';
 import { useState } from 'react';
 
 const useDropDownAction = ({ anchorEl, setAnchorEl, state }) => {
@@ -13,28 +13,35 @@ const useDropDownAction = ({ anchorEl, setAnchorEl, state }) => {
     };
     const handleClose = (e) => {
         const content = e.currentTarget.innerText;
-
+        console.log('....?');
+        console.log(content);
         switch (content) {
             case dropDownMenu.like:
-                console.log('like');
                 likeRequest(`${apiUrl.like}${data.type}s`, {
                     data: {
-                        id: data.id
-                    }
+                        id: data.id,
+                    },
                 });
                 state.setIsLiked(1);
                 break;
             case dropDownMenu.unlike:
                 state.setIsLiked(0);
-                unlikeRequest(`${apiUrl.like}${data.type}/${data.id}`)
+                unlikeRequest(`${apiUrl.like}${data.type}/${data.id}`);
                 break;
             case dropDownMenu.addToPlaylist:
+                request(apiUrl.libraryPlaylist).then((data) => {
+                    state.setPlaylistModal({
+                        visibility: true,
+                        data: data,
+                    });
+                });
                 break;
             case dropDownMenu.addToLibrary:
                 break;
             case dropDownMenu.addToUpNext:
                 break;
             case dropDownMenu.showLyric:
+                state.setDisplayLyrics(true);
                 break;
             case dropDownMenu.buyMP3:
                 break;
