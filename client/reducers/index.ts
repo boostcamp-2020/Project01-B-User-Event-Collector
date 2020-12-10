0301fb8db6b31ade58a8c1a71f6efc253f1e113a
YimJiYoung <chiy323@gmail.com>
2020-12-10 18:03:37 +0900

    [FE] Feat: user reducer 정의 및 초기 데이터 설정
    - user reducer 정의 및 root reducer와 결합
    - user 초기 데이터 설정

--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -1,19 +1,23 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
+import user from './user';
 
 // (아전상태, 액션) => 다음상태
 const rootReducer = combineReducers({
-    index: (state : object = {} , action) => {   //HYDRATE를 위한 index reducer (HYDRATE는 redux의 서버사이드 렌더링을 위해 필요함)
-    switch (action.type) {
-        case HYDRATE:
-            console.log('HYDRATE', action);
-            return {
-                ...state, ...action.payload
-            }
-        default:
-            return state;
+    index: (state: object = {}, action) => {
+        //HYDRATE를 위한 index reducer (HYDRATE는 redux의 서버사이드 렌더링을 위해 필요함)
+        switch (action.type) {
+            case HYDRATE:
+                console.log('HYDRATE', action);
+                return {
+                    ...state,
+                    ...action.payload,
+                };
+            default:
+                return state;
         }
     },
-}) ;
+    user,
+});
 
 export default rootReducer;
