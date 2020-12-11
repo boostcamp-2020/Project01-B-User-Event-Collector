c59f4f168e5360048c883949e7a482907040bdad
shany <shany6310@gmail.com>
2020-12-11 11:42:17 +0900

    [FE] style: 오타 및 띄어씌기 수정 #369
    - 드롭다운 메뉴 컨텐츠 띄어쓰기 및 오타 수정

--- client/constants/dropDownMenu.ts
+++ client/constants/dropDownMenu.ts
@@ -2,10 +2,10 @@
 const dropDownMenu = {
     like:'좋아요',
     unlike:'좋아요 취소',
-    addToPlaylist:'내 플레이리스트에 추가',
+    addToPlaylist:'내 플레이리스트 추가',
     addToLibrary:'보관함에 추가',
-    addToUpNext:'현재 재생목록에 추가',
-    showLyric:'가사보기',
+    addToUpNext:'현재재생목록에 추가',
+    showLyric:'가사 보기',
     buyMP3:'MP3 구매',
     share:'공유'
 }
