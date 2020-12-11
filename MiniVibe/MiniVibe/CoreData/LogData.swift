f8856df82aff41ebfe42731d68ac86fc24cd205b
Sue Cho <suejeong97@naver.com>
2020-12-11 22:16:06 +0900

    [iOS] Fix: Core Data에 custom type이 저장되지 않는 문제 해결 #361
    - NSCoding 채택 후 encoder 구현

--- MiniVibe/MiniVibe/CoreData/LogData.swift
+++ MiniVibe/MiniVibe/CoreData/LogData.swift
@@ -4,19 +4,24 @@
 //
 //  Created by TTOzzi on 2020/12/11.
 //
-
 import Foundation
-
-public final class LogData: NSSecureUnarchiveFromDataTransformer, Encodable {
-    let type: String
-    let id: Int
-    
-    init(type: String, id: Int) {
+@objc(LogData)
+public final class LogData: NSSecureUnarchiveFromDataTransformer, NSCoding, Encodable {
+    public let type: String
+    public let id: Int
+    public init(type: String, id: Int) {
         self.type = type
         self.id = id
     }
+    public required init?(coder: NSCoder) {
+        type = (coder.decodeObject(forKey: "type") as? String) ?? "none"
+        id = coder.decodeInteger(forKey: "id")
+    }
+    public func encode(with coder: NSCoder) {
+        coder.encode(type, forKey: "type")
+        coder.encode(id, forKey: "id")
+    }
 }
-
 extension LogData {
     public override var description: String {
         return "\(type)-\(id)"
