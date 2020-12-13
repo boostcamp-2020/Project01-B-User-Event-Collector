01941adfb88b483d5d7468f79843543692d148c3
shany <shany6310@gmail.com>
2020-12-14 03:07:23 +0900

    [FE] fix: heart 컴포넌트 props type 및 랜더 방식 수정 #409
    - 뮤직플레이어 외의 컴포넌트에서 좋아요 동작을 구현하기 위해 랜더링 방식 수정
    - 상위 컴포넌트로 부터 onclick 함수를 전달받아 좋아요 및 좋아요 취소 동작 구현
    - 좋아요 여부에 따라 하트 색깔 변경되도록 수정

--- client/components/atoms/Heart/Heart.tsx
+++ client/components/atoms/Heart/Heart.tsx
@@ -6,25 +6,30 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 interface HeartProps {
     isSelected: boolean;
-    sort?: 'musicPlayer'
+    sort?: 'musicPlayer';
+    onClick?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
-const Container = styled.div`
-`;
+const Container = styled.div``;
 
-const Heart = ({ isSelected, sort }: HeartProps) => {
+const Heart = ({ isSelected, sort, onClick }: HeartProps) => {
     const [isClicked, setIsClicked] = useState(false);
     const onClickHandler = () => {
         setIsClicked(!isClicked);
-    }
-    return(
-        <Container onClick = { onClickHandler }>
-            {isClicked && sort !== 'musicPlayer' && <FavoriteIcon style={{ color: '#FF1150' }}/>}
-            {!isClicked && sort !== 'musicPlayer' && <FavoriteBorderIcon/>}
-            {isClicked && sort === 'musicPlayer' && <FavoriteIcon style={{ color: '#FF1150' }} fontSize = "small" />}
-            {!isClicked && sort === 'musicPlayer' && <FavoriteBorderIcon style={{ color: '#FFFFFF' }} fontSize = "small"/>}
+    };
+    return (
+        <Container onClick={onClickHandler}>
+            {sort !== 'musicPlayer' ? (
+                <div onClick={onClick}>
+                    {isSelected ? <FavoriteIcon style={{ color: '#FF1150' }} /> : <FavoriteBorderIcon />}
+                </div>
+            ) : isClicked ? (
+                <FavoriteIcon style={{ color: '#FF1150' }} fontSize="small" />
+            ) : (
+                <FavoriteBorderIcon style={{ color: '#FFFFFF' }} fontSize="small" />
+            )}
         </Container>
-        )
+    );
 };
 
 export default Heart;
