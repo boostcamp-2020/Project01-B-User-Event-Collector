e11e1f907e95a07b013e0041e4c4e88df2a33426
shany <shany6310@gmail.com>
2020-12-13 22:46:57 +0900

    [BE] feat: jwt 관련 passport 인증 전략 구현 #405
    - jwt 토큰 정보를 이용하여 vibe DB의 데이터와 비교해 인증된 유저인지 확인
    - 미들웨어로 만들어 확인 후 request에 유저정보 전달

--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -3,14 +3,21 @@ import passport from 'passport';
 import jwt from 'jsonwebtoken';
 
 const router = express.Router();
-
+export const authenticateJWT = (req : Request, res: Response, next: Function) => {
+    passport.authenticate('jwt', { session: false }, (err, user) => {
+        if (err || !user) return res.status(403).json({ success: false });
+        req.user = user.id;
+        return next();
+    })(req, res, next);
+};
 export const authenticateLogin = (req : Request, res : Response, next: Function) => {
     passport.authenticate('login-naver', { session: false }, (err, user) => {
         if (err || !user) return res.status(401).json({ success: false });
-        const token = jwt.sign({ ...user.dataValues }, process.env.JWT_SECRET || 'jwt_secret');
+        const token = jwt.sign({ id: user.id, name: user.name }, process.env.JWT_SECRET || 'jwt_secret');
         res.cookie('token', token);
         return res.status(200).redirect('http://localhost:3000');
     })(req, res, next);
 };
 
+router.get('/naver/callback', authenticateLogin);
 export default router;
