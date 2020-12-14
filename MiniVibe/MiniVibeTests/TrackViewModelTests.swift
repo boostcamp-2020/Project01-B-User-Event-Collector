8e047b0e693280475177a47055d82ce4b1e691b7
Sue Cho <suejeong97@naver.com>
2020-12-14 22:17:12 +0900

    [iOS] Test: TrackViewModel 테스트코드 작성 #419, #420

---
+++ MiniVibe/MiniVibeTests/TrackViewModelTests.swift
@@ -0,0 +1,82 @@
+//
+//  TrackViewModelTests.swift
+//  MiniVibeTests
+//
+//  Created by Sue Cho on 2020/12/14.
+//
+
+import XCTest
+import Combine
+@testable import EventLogKit
+@testable import MiniVibe
+
+class TrackViewModelTests: XCTestCase {
+    
+    struct MockTrackUseCase: TrackUseCaseType {
+        let track: TrackInfo
+        
+        func loadTrack(id: Int) -> AnyPublisher<TrackInfo, UseCaseError> {
+            Just(track)
+                .setFailureType(to: UseCaseError.self)
+                .eraseToAnyPublisher()
+        }
+
+        let success = true
+        func likeTrack(id: Int) -> AnyPublisher<Bool, UseCaseError> {
+            Just(success)
+                .setFailureType(to: UseCaseError.self)
+                .eraseToAnyPublisher()
+        }
+
+        func cancelLikedTrack(id: Int) -> AnyPublisher<Bool, UseCaseError> {
+            Just(success)
+                .setFailureType(to: UseCaseError.self)
+                .eraseToAnyPublisher()
+        }
+    }
+    
+    struct MockEventLogger: EventLoggerType {
+        func send(_ event: EventLogType) {
+            
+        }
+    }
+
+    func test_like_track() {
+        let track = TrackInfo(id: 0,
+                              title: "",
+                              lyrics: "",
+                              albumId: 0,
+                              album: .init(id: 0,
+                                           title: "",
+                                           imageUrl: ""),
+                              artist: .init(id: 0, name: ""),
+                              liked: 0)
+        let usecase = MockTrackUseCase(track: track)
+        let eventLogger = MockEventLogger()
+        let viewModel = TrackViewModel(track: track,
+                                       useCase: usecase,
+                                       eventLogger: eventLogger)
+        viewModel.send(.like)
+        XCTAssertEqual(viewModel.state.track.liked, 1)
+    }
+    
+    func test_cancel_like_track() {
+        let track = TrackInfo(id: 0,
+                              title: "",
+                              lyrics: "",
+                              albumId: 0,
+                              album: .init(id: 0,
+                                           title: "",
+                                           imageUrl: ""),
+                              artist: .init(id: 0, name: ""),
+                              liked: 1)
+        let usecase = MockTrackUseCase(track: track)
+        let eventLogger = MockEventLogger()
+        let viewModel = TrackViewModel(track: track,
+                                       useCase: usecase,
+                                       eventLogger: eventLogger)
+        viewModel.send(.cancelLike)
+        XCTAssertEqual(viewModel.state.track.liked, 0)
+    }
+
+}
