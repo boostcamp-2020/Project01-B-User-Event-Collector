04bdb05c68813cd9a0fef4d8d24179530f5c9a49
shany <shany6310@gmail.com>
2020-12-16 01:01:48 +0900

    [FE] feat: 앨범 상세페이지 체크, '+' 버튼 동작 구현 #449
    - 앨범 상세페이지에서 체크, '+' 표시에 따라 보관함에 저장 및 삭제 한다

--- client/components/atoms/Button/AddButton.tsx
+++ client/components/atoms/Button/AddButton.tsx
@@ -1,7 +1,10 @@
-import React, { MouseEvent, useState } from 'react';
+import React, { MouseEvent, useState, useContext } from 'react';
 import styled from 'styled-components';
 import CheckIcon from '@material-ui/icons/Check';
 import AddIcon from '@material-ui/icons/Add';
+import ComponentInfoContext from '@utils/context/ComponentInfoContext';
+import apiUrl from '@constants/apiUrl';
+import { addToLibrary, deleteFromLibrary } from '@utils/apis';
 
 interface AddButtonProps {
     onClick?: (e: MouseEvent<HTMLElement>) => void;
@@ -13,18 +16,24 @@ const Container = styled.div`
     margin-left: 10px;
 `;
 
-const AddButton = ({
-  onClick, variant
-}: AddButtonProps) => {
+const AddButton = ({ onClick, variant }: AddButtonProps) => {
     const [isClicked, setIsClicked] = useState(false);
+    const componentInfo = useContext(ComponentInfoContext);
+
     const onClickHandler = () => {
+        if (isClicked) {
+            addToLibrary(`${apiUrl.libraryAlbum}`, { data: { id: componentInfo.data.id } });
+        } else {
+            deleteFromLibrary(`${apiUrl.libraryAlbum}/${componentInfo.data.id}`);
+        }
         setIsClicked(!isClicked);
-    }
-    return(
-        <Container onClick = { onClickHandler }>
+    };
+    return (
+        <Container onClick={onClickHandler}>
             {isClicked && <AddIcon />}
             {!isClicked && <CheckIcon />}
         </Container>
-        )};
+    );
+};
 
 export default AddButton;
