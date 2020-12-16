4f4cb2bab5f31e74463746cdc64e2226bb2d1971
shany <shany6310@gmail.com>
2020-12-17 01:27:46 +0900

    [FE] fix: 로그인 실패 알림 후 쿠키 삭제 #460
    - 로그인 실패 알림 후 쿠키가 남아있는 상황을 방지하기 위해 바로 쿠키 삭제

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -33,6 +33,7 @@ const Login = () => {
     useEffect(() => {
         if (cookies.get('fail')) {
             alert('아이디와 비밀번호를 확인해주세요');
+            cookies.remove('fail');
         }
     }, []);
 
