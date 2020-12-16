be7d4a0f2bce968ca06bf6ca69afb6cbeef8a7fe
YimJiYoung <chiy323@gmail.com>
2020-12-16 14:45:35 +0900

    [FE] Feat: PlayButton에서 재생목록 추가 이벤트 수집 #425
    - useUpnextChangeEventLog 훅 이용해서 이벤트 수집
    - tracks 없을 시 early return, optional chaing으로 수정

--- client/components/molecules/PlayButton/PlayButton.tsx
+++ client/components/molecules/PlayButton/PlayButton.tsx
@@ -1,64 +1,66 @@
-import React from 'react'
+import React from 'react';
 import PlayTriangle from '../../atoms/PlayTriangle/PlayTriangle';
 import { StyledPlayButton, StyledCircle } from './PlayButton.styles';
 import { requestTracks } from '@utils/apis';
 import apiUrl from '@constants/apiUrl';
-import { withCookies, Cookies } from 'react-cookie';
+import { Cookies } from 'react-cookie';
 import { useSelector, useDispatch } from 'react-redux';
-import { addToUpNext, addToUpNextAndPlay } from 'reducers/musicPlayer';
+import { addToUpNextAndPlay } from 'reducers/musicPlayer';
+import useUpNextChangeEventLog from '@hooks/useUpNextChangeEventLog';
 
 interface playButtonProps {
     sort?: string;
     data?;
 }
 
-const PlayButton = ({sort, data}) => {
-    const cookies = new Cookies();      //request를 위해 새 쿠키 생성
+const PlayButton = ({ sort, data }) => {
+    const cookies = new Cookies(); //request를 위해 새 쿠키 생성
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user);
+    const { logAddToUpnextEvent } = useUpNextChangeEventLog({ userId: user.id });
 
-    const onClickHandler = async () => {
-        if(user.isLoggedIn){
-        let res; let tracks;
+    const handleClick = async () => {
+        if (!user.isLoggedIn) return;
+
+        let res, tracks;
         switch (sort) {
             case 'todayMagazine':
                 res = await requestTracks(`${apiUrl.magazine}/${data.id}`, cookies.get('token'));
-                tracks = res.playlist.tracks;
-                dispatch(addToUpNextAndPlay(tracks));
+                tracks = res?.playlist?.tracks;
                 break;
             case 'playlist':
                 res = await requestTracks(`${apiUrl.playlist}/${data.id}`, cookies.get('token'));
-                tracks = res.tracks;
-                dispatch(addToUpNextAndPlay(tracks));
+                tracks = res?.tracks;
                 break;
             case 'news':
                 res = await requestTracks(`${apiUrl.album}/${data.albumId}`, cookies.get('token'));
-                tracks = res.tracks;
-                dispatch(addToUpNextAndPlay(tracks));
+                tracks = res?.tracks;
                 break;
             case 'album':
                 res = await requestTracks(`${apiUrl.album}/${data.id}`, cookies.get('token'));
-                tracks = res.tracks;
-                dispatch(addToUpNextAndPlay(tracks));
+                tracks = res?.tracks;
                 break;
             case 'mixtape':
                 res = await requestTracks(`${apiUrl.playlist}/${data.id}`, cookies.get('token'));
-                tracks = res.tracks;
-                dispatch(addToUpNextAndPlay(tracks));
+                tracks = res?.tracks;
                 break;
             case 'station':
                 break;
             default:
                 return;
-        }}
-    }
+        }
+        if (!tracks || tracks.length === 0) return;
 
-    return(
-    <StyledPlayButton onClick={onClickHandler}>
-        <StyledCircle />
-        <PlayTriangle />
-    </StyledPlayButton>
-    )
+        dispatch(addToUpNextAndPlay(tracks));
+        logAddToUpnextEvent(tracks.map(({ id }) => id));
+    };
+
+    return (
+        <StyledPlayButton onClick={handleClick}>
+            <StyledCircle />
+            <PlayTriangle />
+        </StyledPlayButton>
+    );
 };
 
 export default PlayButton;
