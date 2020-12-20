d3ba1f6ea6b4cfb031982f376003555b29144061
shany <shany6310@gmail.com>
2020-12-20 20:12:45 +0900

    [FE] fix: timestamp 현재 시간으로 찍힐 수 있도록 수정
    - 이벤트 발생 시 timestamp가 현재 시간으로 찍힐 수 있도록 함수 작성 및 적용

--- client/utils/eventLogger.ts
+++ client/utils/eventLogger.ts
@@ -1,12 +1,13 @@
 import { sendEvent, sendPlayEvent } from './apis';
 import { Event, PlayEvent } from '@constants/events';
+import { getCurrentDate } from './time';
 
 function eventLogger(event, param) {
     const eventData = {
         event,
         ...param,
         platform: 'Web',
-        timestamp: new Date(),
+        timestamp: getCurrentDate(),
     };
     if (Object.values(Event).indexOf(event) !== -1) sendEvent(eventData);
     else if (Object.values(PlayEvent).indexOf(event) !== -1) sendPlayEvent(eventData);
