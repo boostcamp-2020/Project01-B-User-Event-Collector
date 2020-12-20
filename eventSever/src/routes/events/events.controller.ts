332b0b7fc8c2ae95296de439b434bb4be8fac4a3
shany <shany6310@gmail.com>
2020-12-20 17:51:04 +0900

    [BE] fix: sever 에러 일 경우 메세지 추가 #478

--- eventSever/src/routes/events/events.controller.ts
+++ eventSever/src/routes/events/events.controller.ts
@@ -8,12 +8,12 @@ export const create = async (req: Request, res: Response, next: NextFunction) =>
         await Event.create(event);
     } catch (err) {
         switch (err.name) {
-        case 'ValidationError':
-            return res.status(400).json({ success: false, message: 'Invalid Parameters' });
-        case 'MongooseError':
-            return res.status(400).json({ success: false, message: 'Invalid Event name' });
-        default:
-            return res.status(500).json({ success: false });
+            case 'ValidationError':
+                return res.status(400).json({ success: false, message: 'Invalid Parameters' });
+            case 'MongooseError':
+                return res.status(400).json({ success: false, message: 'Invalid Event name' });
+            default:
+                return res.status(500).json({ success: false, message: 'Sever is something wrong' });
         }
     }
 
--- eventSever/src/routes/playEvents/playEvents.controller.ts
+++ eventSever/src/routes/playEvents/playEvents.controller.ts
@@ -8,12 +8,12 @@ export const create = async (req: Request, res: Response, next: NextFunction) =>
         await Event.create(event);
     } catch (err) {
         switch (err.name) {
-        case 'ValidationError':
-            return res.status(400).json({ success: false, message: 'Invalid Parameters' });
-        case 'MongooseError':
-            return res.status(400).json({ success: false, message: 'Invalid Event name' });
-        default:
-            return res.status(500).json({ success: false });
+            case 'ValidationError':
+                return res.status(400).json({ success: false, message: 'Invalid Parameters' });
+            case 'MongooseError':
+                return res.status(400).json({ success: false, message: 'Invalid Event name' });
+            default:
+                return res.status(500).json({ success: false, message: 'Sever is something wrong' });
         }
     }
 
@@ -23,23 +23,20 @@ export const create = async (req: Request, res: Response, next: NextFunction) =>
 };
 
 export const list = async (req: Request, res: Response, next: NextFunction) => {
-
     try {
-        const data = await Event.find({}).sort({timestamp:-1});
+        const data = await Event.find({}).sort({ timestamp: -1 });
         res.json({
             success: true,
-            data
+            data,
         });
     } catch (err) {
         switch (err.name) {
-        case 'ValidationError':
-            return res.status(400).json({ success: false, message: 'Invalid Parameters' });
-        case 'MongooseError':
-            return res.status(400).json({ success: false, message: 'Invalid Event name' });
-        default:
-            return res.status(500).json({ success: false });
+            case 'ValidationError':
+                return res.status(400).json({ success: false, message: 'Invalid Parameters' });
+            case 'MongooseError':
+                return res.status(400).json({ success: false, message: 'Invalid Event name' });
+            default:
+                return res.status(500).json({ success: false });
         }
     }
-
-
 };
